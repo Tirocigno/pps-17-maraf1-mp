29f24ef9771858b721b1e27fd85f3148959df463
Federico <federico.naldini3@studio.unibo.it>
2018-09-16 16:48:31 +0200

    Define seed objects and card functions.

--- src/main/scala/it/unibo/pps2017/core/deck/cards/Card.scala
+++ src/main/scala/it/unibo/pps2017/core/deck/cards/Card.scala
@@ -1,5 +1,15 @@
+
 package it.unibo.pps2017.core.deck.cards
 
-trait Card {
+import it.unibo.pps2017.core.deck.cards.Seed.Seed
 
+/**
+  * This trait define the basic behaviour of a card.
+  * A card has a seed and a value.
+  */
+
+trait Card {
+  def seed: Seed
+
+  def value: Int
 }
