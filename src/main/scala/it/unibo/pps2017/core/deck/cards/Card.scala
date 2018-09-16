dd880bd2e5cc82881ea4760f278d2c7c493eb085
Federico <federico.naldini3@studio.unibo.it>
2018-09-16 17:16:31 +0200

    Implemented > method on Card

--- src/main/scala/it/unibo/pps2017/core/deck/cards/Card.scala
+++ src/main/scala/it/unibo/pps2017/core/deck/cards/Card.scala
@@ -8,8 +8,33 @@ import it.unibo.pps2017.core.deck.cards.Seed.Seed
   * A card has a seed and a value.
   */
 
-trait Card {
-  def seed: Seed
+sealed trait Card {
 
-  def value: Int
+  private[this] val minValue = 4
+
+  def cardSeed: Seed
+
+  def cardValue: Int
+
+  /**
+    * This method is used to compare two cards.
+    *
+    * @param otherCard the other card to compare.
+    * @return true if the value on the card is greater than the other one, false otherwise.
+    */
+  //noinspection ScalaStyle
+  def >(otherCard: Card): Boolean = otherCard match {
+    case CardImpl(_, otherValue) if otherValue < minValue && cardValue < minValue => cardValue > otherValue
+    case CardImpl(_, otherValue) if otherValue < minValue && cardValue > minValue => false
+    case CardImpl(_, otherValue) if otherValue > minValue && cardValue < minValue => true
+    case CardImpl(_, otherValue) if otherValue > minValue && cardValue > minValue => cardValue > otherValue
+  }
 }
+
+/**
+  * Basic implementation of card.
+  *
+  * @param cardSeed  the seed of the card.
+  * @param cardValue the value of the card.
+  */
+case class CardImpl(override val cardSeed: Seed, override val cardValue: Int) extends Card
