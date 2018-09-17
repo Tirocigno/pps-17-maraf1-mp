f4c344b13ec8bea60fd3342982905fb6c7cd5993
Tirocigno <federico.naldini3@studio.unibo.it>
2018-09-17 15:27:45 +0200

    Implemented shuffle method

--- src/main/scala/it/unibo/pps2017/core/deck/SimpleDeck.scala
+++ src/main/scala/it/unibo/pps2017/core/deck/SimpleDeck.scala
@@ -1,12 +1,16 @@
 
 package it.unibo.pps2017.core.deck
 
+import it.unibo.pps2017.core.deck.cards.Card
+
 /**
   * Trait to implement a simple deck used in a game.
   * A Deck is a collection of 40 different cards that can be shuffled, split in
   * four different hands of card.
   */
 trait SimpleDeck {
+
+  private[deck] var cardList: Seq[Card]
 
   /**
     * This method shuffle the order of the cards inside the deck.
@@ -16,7 +20,15 @@ trait SimpleDeck {
   /**
     * Split the deck of card inside in four random collections of cards of the same size.
     *
-    * @return
+    * @return a sequence containing four hands of cards.
     */
   def distribute(): Seq[CardsHand]
+}
+
+
+private class SimpleDeckImpl(override var cardList: Seq[Card]) extends SimpleDeck {
+
+  override def shuffle(): Unit = cardList.shuffle()
+
+  override def distribute(): Seq[CardsHand] = ???
 }
