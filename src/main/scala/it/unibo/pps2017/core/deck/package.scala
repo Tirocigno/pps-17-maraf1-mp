7720a4e431d2375dd45754a717f51bb522146e9a
Tirocigno <federico.naldini3@studio.unibo.it>
2018-09-17 15:27:45 +0200

    Implemented shuffle method

--- src/main/scala/it/unibo/pps2017/core/deck/package.scala
+++ src/main/scala/it/unibo/pps2017/core/deck/package.scala
@@ -3,7 +3,21 @@ package it.unibo.pps2017.core
 
 import it.unibo.pps2017.core.deck.cards.Card
 
+import scala.util.Random
+
 package object deck {
 
+  /**
+    * Alias for a card hand.
+    */
   type CardsHand = Iterable[Card]
+
+  /**
+    * Implicit class to randomize a Sequence of cards.
+    *
+    * @param cardSequence the card sequence to shuffle.
+    */
+  implicit class RandomizeCardSequence(var cardSequence: Seq[Card]) {
+    def shuffle(): Unit = cardSequence = Random.shuffle(cardSequence)
+  }
 }
