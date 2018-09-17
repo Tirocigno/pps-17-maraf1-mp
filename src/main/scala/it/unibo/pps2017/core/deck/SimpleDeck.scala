47d131aed972eb05517d2c63fe431232441ecc99
Tirocigno <federico.naldini3@studio.unibo.it>
2018-09-17 14:22:05 +0200

    Created SimpleDeck trait

---
+++ src/main/scala/it/unibo/pps2017/core/deck/SimpleDeck.scala
@@ -0,0 +1,11 @@
+
+package it.unibo.pps2017.core.deck
+
+/**
+  * Trait to implement a simple deck used in a game.
+  * A Deck is a collection of 40 different cards that can be shuffled, split in
+  * four different hands of card.
+  */
+trait SimpleDeck {
+
+}
--- src/main/scala/it/unibo/pps2017/demo/ScalaDemo.scala
+++ src/main/scala/it/unibo/pps2017/demo/ScalaDemo.scala
@@ -1,3 +1,4 @@
+
 package it.unibo.pps2017.demo
 
 object ScalaDemo extends App {
