7dbabc6eb6c94af5c0c18f8ca0d5ef889dffd5e7
Tirocigno <fede.naldini1@gmail.com>
2018-09-17 16:07:32 +0200

    Fixed range of the min/max value for the stream. Test now pass

--- src/test/scala/it/unibo/pps2017/core/deck/SimpleDeckTest.scala
+++ src/test/scala/it/unibo/pps2017/core/deck/SimpleDeckTest.scala
@@ -8,6 +8,9 @@ import org.scalatest.junit.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class SimpleDeckTest extends FunSuite {
 
+  /**
+    * Test to check the size of the default deck.
+    */
   test("Size of Default deck") {
     assert(SimpleDeck.generateDefaultCardsList().size == defaultDeckSize)
   }
