5c11dd01a885c0c2bc07a883656461357d5318b3
Tirocigno <fede.naldini1@gmail.com>
2018-09-20 12:39:13 +0200

    Created ScoreCounter test

---
+++ src/test/scala/it/unibo/pps2017/core/deck/ScoreCounterTest.scala
@@ -0,0 +1,11 @@
+
+package it.unibo.pps2017.core.deck
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class ScoreCounterTest extends FunSuite {
+
+}
