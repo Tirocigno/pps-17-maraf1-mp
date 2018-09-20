2a3a53c83d96905ac6b1968d4315fcadb90e0090
Nicholas <nicholas.brasini@studio.unibo.it>
2018-09-20 12:59:53 +0200

    Add main class and his fxml's file

--- src/main/java/it/unibo/pps2017/core/gui/Main.java
+++ src/main/java/it/unibo/pps2017/core/gui/Main.java
@@ -1,5 +1,28 @@
 package it.unibo.pps2017.core.gui;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			//BorderPane root = new BorderPane();
+			Parent root = FXMLLoader.load(getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root,700,700);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
---
+++ src/main/java/it/unibo/pps2017/core/gui/MainController.java
@@ -0,0 +1,27 @@
+package it.unibo.pps2017.core.gui;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+
+public class MainController {
+
+	@FXML
+	ImageView immagine;
+	
+	@FXML
+	ImageView firstCard;
+	
+	@FXML
+	ImageView secondCard;
+	
+	@FXML
+	Button button;
+	
+	public void getNumber(ActionEvent e) {
+		firstCard.setVisible(true);
+		secondCard.setVisible(true);
+	}
+}
