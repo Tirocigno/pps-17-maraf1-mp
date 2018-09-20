3c5a7fb27b6a99c38a8dffabf0cf1ec59ffb3d7b
Tirocigno <fede.naldini1@gmail.com>
2018-09-20 13:33:21 +0200

    Written second test, not working

--- src/test/scala/it/unibo/pps2017/core/deck/ScoreCounterTest.scala
+++ src/test/scala/it/unibo/pps2017/core/deck/ScoreCounterTest.scala
@@ -1,15 +1,28 @@
 
 package it.unibo.pps2017.core.deck
 
+import it.unibo.pps2017.core.deck.cards.{Card, CardImpl, Seed}
 import org.junit.runner.RunWith
-import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfter, FunSuite}
 
 @RunWith(classOf[JUnitRunner])
-class ScoreCounterTest extends FunSuite {
+class ScoreCounterTest extends FunSuite with BeforeAndAfter {
+
+  var scoreCounter: ScoreCounter = ScoreCounter()
+
+  private[this] def generateCard(cardValue: Int): Card = new CardImpl(Seed.Club, cardValue)
+
+  before {
+    scoreCounter = ScoreCounter()
+  }
 
   test("Creation of a ScoreCounter object") {
-    val scoreCounter: ScoreCounter = ScoreCounter()
+    assert(scoreCounter.scores == (0, 0))
+  }
+
+  test("Register a card with no value") {
+    scoreCounter.registerPlayedCard(generateCard(nullValueCard), FirstTeam)
     assert(scoreCounter.scores == (0, 0))
   }
 
