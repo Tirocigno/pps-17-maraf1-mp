7c4cee4cb581e37f4efc703ace96b49f940bd74b
Tirocigno <fede.naldini1@gmail.com>
2018-09-24 17:46:23 +0200

    Refactored all ScoreCounter test.

--- src/test/scala/it/unibo/pps2017/core/deck/ScoreCounterTest.scala
+++ src/test/scala/it/unibo/pps2017/core/deck/ScoreCounterTest.scala
@@ -12,42 +12,45 @@ class ScoreCounterTest extends FunSuite with BeforeAndAfter {
   var scoreCounter: ScoreCounter = ScoreCounter()
   private[this] val highCardWithValue = 8
   private[this] val lowCardWithValue = 2
-  private[this] val nullValueCard: Int = 5
+  private[this] val noCardValue: Int = 5
 
   private[this] def generateCard(cardValue: Int): Card = CardImpl(Seed.Club, cardValue)
+
+  private def generateOneScoreHand =
+    Seq(generateCard(lowCardWithValue), generateCard(highCardWithValue),
+      generateCard(noCardValue), generateCard(lowCardWithValue))
+
+  private def generateNoScoreHand =
+    Stream.range(0, handSize).map(_ => generateCard(noCardValue))
+
+  private def generateMixedHand = Seq(generateCard(aceValue), generateCard(lowCardWithValue),
+    generateCard(highCardWithValue), generateCard(noCardValue))
+
+  private def registerHandAndCheckScore(handToRegister: Seq[Card])(expectedScore: Int) = {
+    scoreCounter.registerSetPlayedCards(handToRegister, firstTeam)
+    assert(scoreCounter.computeSetScore() == (expectedScore + 1, 0))
+  }
 
   before {
     scoreCounter = ScoreCounter()
   }
 
-  test("Creation of a ScoreCounter object") {
-    assert(scoreCounter.scores == (0, 0))
+  test("Initial test status") {
+    assert(scoreCounter.computeSetScore() == (0, 0))
   }
 
-  test("Register a card with no value") {
-    scoreCounter.registerPlayedCard(generateCard(nullValueCard), FirstTeam)
-    assert(scoreCounter.scores == (0, 0))
+  test("Register one score hand with figures") {
+    registerHandAndCheckScore(generateOneScoreHand)(1)
   }
 
-  test("Register a ace") {
-    scoreCounter.registerPlayedCard(generateCard(aceValue), FirstTeam)
-    assert(scoreCounter.scores == (1, 0))
+  test("Register zero score hand") {
+    registerHandAndCheckScore(generateNoScoreHand)(0)
   }
 
-  test("Register three low cards with values") {
-    for (_ <- 0 to 3) scoreCounter.registerPlayedCard(generateCard(lowCardWithValue), FirstTeam)
-    assert(scoreCounter.scores == (1, 0))
+  test("Register mixed hand") {
+    registerHandAndCheckScore(generateMixedHand)(1)
   }
 
-  test("Register three high cards with values") {
-    for (_ <- 0 to 3) scoreCounter.registerPlayedCard(generateCard(highCardWithValue), FirstTeam)
-    assert(scoreCounter.scores == (1, 0))
-  }
 
-  test("Finish a set") {
-    scoreCounter.registerPlayedCard(generateCard(aceValue), FirstTeam)
-    scoreCounter.finishSet()
-    assert(scoreCounter.scores == (2, 0))
-  }
 
 }
