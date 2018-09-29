896126e3570fc24f9dcb9530b12857d129b58dbb
Gjulio <gjuliojakova@yahoo.com>
2018-09-29 21:15:52 +0200

    Added Controller

--- src/main/java/it/unibo/pps2017/core/player/Controller.java
+++ src/main/java/it/unibo/pps2017/core/player/Controller.java
@@ -2,9 +2,45 @@ package it.unibo.pps2017.core.player;
 
 import it.unibo.pps2017.core.deck.cards.Card;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public interface Controller {
 
-    Set<Card> getHand();
+    Map<String,List<Card>> getAllHands();
+
+    List<Card> getPlayerHand(String player);
+
+    void setHands(List<Card> hand);
+
+    boolean isCardOk(Card c);
+
+    void setTurn(String player);
+
+   // int getCardIndex(Card c);
+
+   // void playCard(Card c);
+
+    //  boolean isFirstPlayedCard(Card c);
+
+   // boolean isLastPlayedCard(Card c);
+
+    void addPlayer(Controller player);
+
+    void cleanField();
+
+   // int indexNextTurn();
+
+   // Set<Card> getInitCards();
+
+   // void setInitCards(Set<Card> cards);
+
+    Card getRandCard();
+
+    void setCommand(String command);
+
+    void totalPoints(Integer pointsTeam1, Integer pointsTeam2);
+
+   // boolean timeExpired();
 }
