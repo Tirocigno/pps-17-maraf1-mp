1826930f7cfc5613935189dfcb354081694e2317
Nicholas <nicholas.brasini@studio.unibo.it>
2018-09-29 11:23:27 +0200

    Deleted resizable

--- src/main/java/it/unibo/pps2017/core/gui/PlayGameView.java
+++ src/main/java/it/unibo/pps2017/core/gui/PlayGameView.java
@@ -14,8 +14,6 @@ public class PlayGameView extends Application {
 			
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			//primaryStage.setResizable(false);
-			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch (Exception e) {
