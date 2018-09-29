0476574c4ce9c354f2a5b55116dbb01c114289c7
Nicholas <nicholas.brasini@studio.unibo.it>
2018-09-29 11:25:35 +0200

    Deleted line of code

--- src/main/java/it/unibo/pps2017/core/gui/PlayGameView.java
+++ src/main/java/it/unibo/pps2017/core/gui/PlayGameView.java
@@ -11,7 +11,6 @@ public class PlayGameView extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("PlayGameView.fxml"));
-			
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
