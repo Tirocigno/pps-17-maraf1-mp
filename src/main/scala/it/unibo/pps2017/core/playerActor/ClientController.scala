78d8087da77f54b7aad1e9831a5585149f19aa92
Nicholas <nicholas.brasini@studio.unibo.it>
2018-10-08 17:34:12 +0200

    Added ClientController class

--- src/main/scala/it/unibo/pps2017/core/playerActor/ClientController.scala
+++ src/main/scala/it/unibo/pps2017/core/playerActor/ClientController.scala
@@ -1,12 +1,40 @@
 package it.unibo.pps2017.core.playerActor
 
 import it.unibo.pps2017.core.gui.PlayGameController
+import it.unibo.pps2017.core.player._
+import it.unibo.pps2017.core.player.Command.Command
+
+import scala.collection.JavaConverters._
+
 
 abstract class ClientController {
 
-  val  playGameController: PlayGameController
+  val playGameController: PlayGameController
   val playerActor: PlayerActor
 
+  def getCardsFirstPlayer(cards: List[String]): Unit = {
+    playGameController.getCardsFirstPlayer(cards.asJava)
+  }
+
+  def getBriscolaChosen(briscola: String): Unit = {
+    playGameController.getBriscolaChosen(briscola)
+  }
+
+  def getCommand(command: Command, player: Player): Unit = {
+    playGameController.getCommand(player, command)
+  }
+
+  def showOtherPlayersPlayedCard(path: String, player: Player): Unit = {
+    playGameController.showOtherPlayersPlayedCard(player, path)
+  }
+
+  def cleanFieldEndTotalTurn(firstTeamScore: Int, secondTeamScore: Int, endMatch: Boolean): Unit = {
+    playGameController.cleanFieldEndTotalTurn(firstTeamScore, secondTeamScore, endMatch)
+  }
+
+  def setCurrentPlayer(player: Player, partialTurnIsEnded: Boolean, isFirstPlayer: Boolean): Unit = {
+   playGameController.setCurrentPlayer(player, partialTurnIsEnded, isFirstPlayer)
+  }
 
 
 
