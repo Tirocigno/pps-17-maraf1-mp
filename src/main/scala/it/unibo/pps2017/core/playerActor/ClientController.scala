4b10358f732b1afdf9058132f083ef7d90414be7
Nicholas <nicholas.brasini@studio.unibo.it>
2018-10-08 16:47:17 +0200

    Added Client Controller

---
+++ src/main/scala/it/unibo/pps2017/core/playerActor/ClientController.scala
@@ -0,0 +1,13 @@
+package it.unibo.pps2017.core.playerActor
+
+import it.unibo.pps2017.core.gui.PlayGameController
+
+abstract class ClientController {
+
+  val  playGameController: PlayGameController
+  val playerActor: PlayerActor
+
+
+
+
+}
