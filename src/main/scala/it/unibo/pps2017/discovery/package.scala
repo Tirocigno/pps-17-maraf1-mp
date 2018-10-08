cf6a2bb0049af3943f9dcd0ebef695e1669b8b26
Tirocigno <fede.naldini1@gmail.com>
2018-10-08 18:59:04 +0200

    Implemented ServiceContext.

--- src/main/scala/it/unibo/pps2017/discovery/package.scala
+++ src/main/scala/it/unibo/pps2017/discovery/package.scala
@@ -5,5 +5,25 @@ package object discovery {
 
   type IPAddress = String
 
+  type Port = Int
+
   type MatchRef = String
+
+  type ServerContext = (IPAddress,Port)
+
+  implicit class RichServerContext(serverContext: ServerContext) {
+    def ipAddress:IPAddress = serverContext._1
+    def port:Port = serverContext._2
+
+    override def hashCode(): Port = super.hashCode()
+
+    override def equals(obj: Any): Boolean = {
+      if(obj.isInstanceOf[ServerContext]) {
+        serverContext.ipAddress.equals(obj.asInstanceOf[ServerContext].ipAddress) &&
+        serverContext.port == obj.asInstanceOf[ServerContext].port
+      } else {
+        false
+      }
+    }
+  }
 }
