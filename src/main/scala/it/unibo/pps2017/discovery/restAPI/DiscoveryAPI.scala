9e647740a02c382ae9346cdc84dc0859fc41df0c
Tirocigno <fede.naldini1@gmail.com>
2018-10-08 18:36:34 +0200

    Updated Scaladoc.

--- src/main/scala/it/unibo/pps2017/discovery/restAPI/DiscoveryAPI.scala
+++ src/main/scala/it/unibo/pps2017/discovery/restAPI/DiscoveryAPI.scala
@@ -7,9 +7,28 @@ import it.unibo.pps2017.server.model.{GET, POST, Request, RouterResponse}
 
 object DiscoveryAPI {
 
+  /**
+    * Trait of DiscoveryAPI.
+    */
   sealed trait DiscoveryAPI {
+    /**
+      * Path of the API
+      * @return a string containing the path of API.
+      */
     def path:String
+
+    /**
+      * Http method of API.
+      * @return an HTTP method to call.
+      */
     def httpMethod:HttpMethod
+
+    /**
+      * Convert the API to a request object to register into a router.
+      * @param router the router on which request will be registered.
+      * @param handle the handler of the request.
+      * @return a Request object build from the API.
+      */
     def asRequest(router: Router, handle:(RoutingContext, RouterResponse) => Unit):Request
   }
 
