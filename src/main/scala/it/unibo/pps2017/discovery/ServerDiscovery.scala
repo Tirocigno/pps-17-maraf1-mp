59b27a9d66451d6a4bfb9a3a5432ee604bb27ede
Tirocigno <fede.naldini1@gmail.com>
2018-10-08 19:22:23 +0200

    Added support to first REST API

--- src/main/scala/it/unibo/pps2017/discovery/ServerDiscovery.scala
+++ src/main/scala/it/unibo/pps2017/discovery/ServerDiscovery.scala
@@ -5,8 +5,9 @@ import io.vertx.lang.scala.ScalaVerticle
 import io.vertx.scala.core.http.HttpServerOptions
 import io.vertx.scala.ext.web.{Router, RoutingContext}
 import it.unibo.pps2017.discovery.restAPI.DiscoveryAPI
+import it.unibo.pps2017.discovery.restAPI.DiscoveryAPI.RegisterServerAPI
 import it.unibo.pps2017.server.controller.Dispatcher.{PORT, TIMEOUT}
-import it.unibo.pps2017.server.model.RouterResponse
+import it.unibo.pps2017.server.model.{Message, RouterResponse}
 
 /**
   * Basic trait for a server discovery implementation.
@@ -25,13 +26,19 @@ object ServerDiscovery {
 
 private class ServerDiscoveryImpl extends ServerDiscovery {
 
+  val serverMap:ServerMap = ???
+  val matchesSet:MatchesSet = MatchesSet()
+
   override def start(): Unit = developAPI()
 
   private def mockHandler:(RoutingContext, RouterResponse) => Unit = (_,_) => println("Api called")
 
   override def developAPI(): Unit = {
     val router = Router.router(vertx)
-    DiscoveryAPI.values.foreach(_.asRequest(router,mockHandler))
+    DiscoveryAPI.values.map({
+      case api @ RegisterServerAPI => api.asRequest(router,getServerAPIHandler)
+      case api @ _ => api.asRequest(router,mockHandler)
+    })
     val options = HttpServerOptions()
     options.setCompressionSupported(true)
       .setIdleTimeout(TIMEOUT)
@@ -39,6 +46,10 @@ private class ServerDiscoveryImpl extends ServerDiscovery {
 
     vertx.createHttpServer(options)
       .requestHandler(router.accept _).listen(PORT)
+  }
+
+  private val getServerAPIHandler:(RoutingContext, RouterResponse) => Unit = (request,response) => {
+    response.sendResponse(Message(serverMap.getLessBusyServer))
   }
 
   override def handleRestCall(): Unit = ???
