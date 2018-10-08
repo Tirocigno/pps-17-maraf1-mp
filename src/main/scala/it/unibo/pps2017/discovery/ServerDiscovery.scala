00cc2468bb087727aeb4198e117e0d73deb497ae
Tirocigno <fede.naldini1@gmail.com>
2018-10-08 18:13:10 +0200

    Deployed APIs

--- src/main/scala/it/unibo/pps2017/discovery/ServerDiscovery.scala
+++ src/main/scala/it/unibo/pps2017/discovery/ServerDiscovery.scala
@@ -1,13 +1,41 @@
 
 package it.unibo.pps2017.discovery
 
+import io.vertx.lang.scala.ScalaVerticle
+import io.vertx.scala.core.http.HttpServerOptions
+import io.vertx.scala.ext.web.{Router, RoutingContext}
+import it.unibo.pps2017.discovery.restAPI.DiscoveryAPI
+import it.unibo.pps2017.server.controller.Dispatcher.{PORT, TIMEOUT}
+import it.unibo.pps2017.server.model.RouterResponse
+
 /**
   * Basic trait for a server discovery implementation.
   */
-trait ServerDiscovery {
+trait ServerDiscovery extends ScalaVerticle{
 
   def developAPI():Unit
 
   def handleRestCall():Unit
 
+}
+
+private class SeverDiscoveryImpl extends ServerDiscovery {
+
+  override def start(): Unit = developAPI()
+
+  private def mockHandler:(RoutingContext, RouterResponse) => Unit = (_,_) => println("Api called")
+
+  override def developAPI(): Unit = {
+    val router = Router.router(vertx)
+    DiscoveryAPI.values.foreach(_.asRequest(router,mockHandler))
+    val options = HttpServerOptions()
+    options.setCompressionSupported(true)
+      .setIdleTimeout(TIMEOUT)
+
+
+    vertx.createHttpServer(options)
+      .requestHandler(router.accept _).listen(PORT)
+  }
+
+  override def handleRestCall(): Unit = ???
 }
--- src/main/scala/it/unibo/pps2017/discovery/restAPI/DiscoveryAPI.scala
+++ src/main/scala/it/unibo/pps2017/discovery/restAPI/DiscoveryAPI.scala
@@ -2,12 +2,15 @@
 package it.unibo.pps2017.discovery.restAPI
 
 import io.vertx.core.http.HttpMethod
+import io.vertx.scala.ext.web.{Router, RoutingContext}
+import it.unibo.pps2017.server.model.{GET, POST, Request, RouterResponse}
 
 object DiscoveryAPI {
 
   sealed trait DiscoveryAPI {
     def path:String
     def httpMethod:HttpMethod
+    def asRequest(router: Router, handle:(RoutingContext, RouterResponse) => Unit):Request
   }
 
   case object RegisterServerAPI extends DiscoveryAPI {
@@ -15,6 +18,9 @@ object DiscoveryAPI {
     override def path: String = "/registerserver"
 
     override def httpMethod: HttpMethod = HttpMethod.POST
+
+    override def asRequest(router: Router, handle: (RoutingContext, RouterResponse) => Unit): Request =
+      POST(router,path,handle)
   }
 
   case object getServerAPI extends DiscoveryAPI {
@@ -22,6 +28,12 @@ object DiscoveryAPI {
     override def path: String = "/getserver"
 
     override def httpMethod: HttpMethod = HttpMethod.GET
+
+    override def asRequest(router: Router, handle: (RoutingContext, RouterResponse) => Unit): Request =
+      GET(router,path,handle)
   }
+
+  def values:Set[DiscoveryAPI] = Set(getServerAPI, RegisterServerAPI)
+
 
 }
