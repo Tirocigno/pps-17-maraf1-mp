859f9c1c82ec9363c6c440990dba25cf5c0e9c0b
Nicholas <nicholas.brasini@studio.unibo.it>
2018-10-08 11:45:48 +0200

    Added messages in companion object of playerActor

--- src/main/scala/it/unibo/pps2017/core/playerActor/PlayerActor.scala
+++ src/main/scala/it/unibo/pps2017/core/playerActor/PlayerActor.scala
@@ -1,14 +1,32 @@
 package it.unibo.pps2017.core.playerActor
 
 import akka.actor.Actor
-import akka.event.Logging
+import it.unibo.pps2017.core.player.Player
+import it.unibo.pps2017.core.playerActor.PlayerActor._
+
+
+
+object PlayerActor {
+  case class DistributedCardMsg (cards: List[String])
+  case class SelectBriscolaMsg()
+  case class TurnMsg(player: Player, endPartialTurn: Boolean, isFirstPlayer: Boolean)
+  case class ClickedCardMsg(index: Int)
+  case class EndTurnMsg(firstTeamScore: Int, secondTeamScore: Int, endMatch: Boolean)
+  case class PlayedCardMsg (path: String, player: Player)
+  case class ClickedCommandMsg(command: String)
+  case class NotifyCommandMsg (command: String, player: Player)
+}
+
 
 class PlayerActor extends Actor {
 
-  val log = Logging(context.system, this)
+  //val log = Logging(context.system, this)
 
   def receive: PartialFunction[Any, Unit] = {
-    case "test" ⇒ log.info("received test")
-    case _      ⇒ log.info("received unknown message")
+
+    case SelectBriscolaMsg() =>
+
   }
+
+
 }
