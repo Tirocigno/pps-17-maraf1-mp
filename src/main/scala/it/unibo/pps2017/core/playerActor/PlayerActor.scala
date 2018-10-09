23239c6604a76fbf9011590e8c07024c14558a84
Nicholas <nicholas.brasini@studio.unibo.it>
2018-10-09 18:33:31 +0200

    Renamed all messages and implemented DistributedCard

--- src/main/scala/it/unibo/pps2017/core/playerActor/PlayerActor.scala
+++ src/main/scala/it/unibo/pps2017/core/playerActor/PlayerActor.scala
@@ -1,61 +1,77 @@
 package it.unibo.pps2017.core.playerActor
 
 import akka.actor.Actor
+import akka.actor.ActorRef
 import it.unibo.pps2017.core.player.Player
 import it.unibo.pps2017.core.player.Command.Command
 import it.unibo.pps2017.core.playerActor.PlayerActor._
+import it.unibo.pps2017.core.deck.cards._
 
 object PlayerActor {
-  case class DistributedCardMsg (cards: List[String])
-  case class SelectBriscolaMsg(briscola: String)
-  case class GetBriscolaChosenMsg(briscola: String)
-  case class TurnMsg(player: Player, endPartialTurn: Boolean, isFirstPlayer: Boolean)
-  case class ClickedCardMsg(index: Int)
-  case class EndTurnMsg(firstTeamScore: Int, secondTeamScore: Int, endMatch: Boolean)
-  case class PlayedCardMsg (path: String, player: Player)
-  case class ClickedCommandMsg(command: String, player: Player)
-  case class NotifyCommandMsg (command: Command, player: Player)
+  case class DistributedCard(cards: Set[Card], player: ActorRef)
+  case class SelectBriscola(briscola: String)
+  case class GetBriscolaChosen(briscola: String)
+  case class Turn(player: Player, endPartialTurn: Boolean, isFirstPlayer: Boolean)
+  case class ClickedCard(index: Int)
+  case class EndTurn(firstTeamScore: Int, secondTeamScore: Int, endMatch: Boolean)
+  case class PlayedCard(path: String, player: Player)
+  case class ClickedCommand(command: String, player: Player)
+  case class NotifyCommand(command: Command, player: Player)
 }
 
 
   class PlayerActor(clientController: ClientController) extends Actor {
 
+    /* Qui dovro' inserire il mio player */
+    var player : Player = _
+
+
   def receive: PartialFunction[Any, Unit] = {
 
-    case DistributedCardMsg(cards) => {
-      clientController.getCardsFirstPlayer(cards)
+    case DistributedCard(cards, player) => {
+
+      /* Se il messaggio che arriva rappresenta le mie carte, le invio al ClientController */
+      if (this.player.eq(player)) {
+        val cards1 = List("")
+        clientController.getCardsFirstPlayer(cards1)
+        /**
+          * Quando Ulio avra' fatto, chiamero':
+          * clientController.getCardsFirstPlayer(cards)
+          */
+      }
+
     }
 
-    case GetBriscolaChosenMsg(briscola) => {
+    case GetBriscolaChosen(briscola) => {
       clientController.getBriscolaChosen(briscola)
     }
 
-    case SelectBriscolaMsg(briscola) => {
+    case SelectBriscola(briscola) => {
       /** inviare briscola scelta al GameActor */
     }
 
-    case ClickedCardMsg(index) => {
+    case ClickedCard(index) => {
       /** inviare l'indice della carta scelta al GameActor */
 
     }
 
-    case ClickedCommandMsg(command, player) => {
+    case ClickedCommand(command, player) => {
       /** inviare comando e giocatore al GameActor */
     }
 
-    case TurnMsg(player, endPartialTurn, isFirstPlayer) => {
+    case Turn(player, endPartialTurn, isFirstPlayer) => {
       clientController.setCurrentPlayer(player, endPartialTurn, isFirstPlayer)
     }
 
-    case EndTurnMsg(firstTeamScore, secondTeamScore, endMatch) => {
+    case EndTurn(firstTeamScore, secondTeamScore, endMatch) => {
       clientController.cleanFieldEndTotalTurn(firstTeamScore, secondTeamScore, endMatch)
     }
 
-    case PlayedCardMsg(path, player) => {
+    case PlayedCard(path, player) => {
       clientController.showOtherPlayersPlayedCard(path, player)
     }
 
-    case NotifyCommandMsg(player, command) => {
+    case NotifyCommand(player, command) => {
       clientController.getCommand(player, command)
     }
 
