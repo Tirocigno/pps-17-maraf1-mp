9edf213f8daf1d2dbdfca9291e86cd65c71a8b99
Tirocigno <fede.naldini1@gmail.com>
2018-10-09 11:33:23 +0200

    Removed useless equals method

--- src/main/scala/it/unibo/pps2017/discovery/package.scala
+++ src/main/scala/it/unibo/pps2017/discovery/package.scala
@@ -14,12 +14,5 @@ package object discovery {
   implicit class RichServerContext(serverContext: ServerContext) {
     def ipAddress:IPAddress = serverContext._1
     def port:Port = serverContext._2
-
-    override def hashCode(): Port = super.hashCode()
-
-    override def equals(obj: Any): Boolean = obj match {
-      case other:RichServerContext => other.ipAddress.equals(serverContext.ipAddress) && other.port == serverContext.port
-      case _ => false
-    }
   }
 }
