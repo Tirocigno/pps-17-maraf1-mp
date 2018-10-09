3b7e2b8c28a486550aeaeefce63ac27d61e14310
Tirocigno <fede.naldini1@gmail.com>
2018-10-09 13:27:59 +0200

    Deployed ServerMap

--- src/main/scala/it/unibo/pps2017/discovery/package.scala
+++ src/main/scala/it/unibo/pps2017/discovery/package.scala
@@ -1,6 +1,8 @@
 
 package it.unibo.pps2017
 
+import it.unibo.pps2017.server.model.JsonResponse
+
 package object discovery {
 
   type IPAddress = String
@@ -11,7 +13,7 @@ package object discovery {
 
   type ServerContext = (IPAddress,Port)
 
-  implicit class RichServerContext(serverContext: ServerContext) {
+  implicit class RichServerContext(serverContext: ServerContext) extends JsonResponse {
     def ipAddress:IPAddress = serverContext._1
     def port:Port = serverContext._2
   }
