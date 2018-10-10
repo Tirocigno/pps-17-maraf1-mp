430a95d135b5e56ad18cd0cf3d231dde3b90f608
Tirocigno <fede.naldini1@gmail.com>
2018-10-10 17:43:06 +0200

    Adapted code to ServerDiscovery changes

--- src/main/scala/it/unibo/pps2017/discovery/MockMain.scala
+++ src/main/scala/it/unibo/pps2017/discovery/MockMain.scala
@@ -5,7 +5,7 @@ import io.vertx.scala.core.Vertx
 
 
 object MockMain extends App {
-  val serverDiscovery = ServerDiscovery()
+  val serverDiscovery = ServerDiscovery(4700, 3)
   serverDiscovery.addMockServer("Autunno", 2018)
   Vertx.vertx().deployVerticle(serverDiscovery)
 }
