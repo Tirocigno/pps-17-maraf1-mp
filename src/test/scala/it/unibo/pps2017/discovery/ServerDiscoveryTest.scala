3cf3260a1c1453a5fa23af3efc2a03a0fc19486e
Tirocigno <fede.naldini1@gmail.com>
2018-10-10 16:54:07 +0200

    Sequentialization of test SEEMS to work.

--- src/test/scala/it/unibo/pps2017/discovery/ServerDiscoveryTest.scala
+++ src/test/scala/it/unibo/pps2017/discovery/ServerDiscoveryTest.scala
@@ -1,8 +1,40 @@
 
 package it.unibo.pps2017.discovery
 
-import org.scalatest.FunSuite
+import io.vertx.scala.core.Vertx
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
 
-class ServerDiscoveryTest extends FunSuite {
+@RunWith(classOf[JUnitRunner])
+class ServerDiscoveryTest extends FunSuite with BeforeAndAfterEach {
+
+  val serverDiscovery: ServerDiscovery = ServerDiscovery()
+  val defaultPort: Int = 8080
+  val otherPort: Int = 8081
+  private val vertx = Vertx.vertx()
+
+  override def beforeEach() {
+    vertx.deployVerticle(serverDiscovery)
+  }
+
+  test("Mock test 1") {
+    Thread.sleep(1000)
+  }
+
+  test("Mock test 2") {
+    Thread.sleep(1000)
+  }
+
+  test("Mock test 3") {
+    Thread.sleep(1000)
+  }
+  test("Mock test 4") {
+    Thread.sleep(1000)
+  }
+
+  override def afterEach() {
+    vertx.close()
+  }
 
 }
