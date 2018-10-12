5927add9f1667331c0a3dd3afb56f91d49613028
Tirocigno <fede.naldini1@gmail.com>
2018-10-12 14:48:24 +0200

    Created matchesSet encoder and decodere

--- src/main/scala/it/unibo/pps2017/server/model/ResponseMessage.scala
+++ src/main/scala/it/unibo/pps2017/server/model/ResponseMessage.scala
@@ -5,6 +5,7 @@ import cats.syntax.functor._
 import io.circe.generic.auto._
 import io.circe.syntax._
 import io.circe.{Decoder, Encoder}
+import it.unibo.pps2017.discovery.MatchRef
 
 /**
   * This class is used for define the message accepted to the RouterResponse.
@@ -19,6 +20,7 @@ case class Message(message: String) extends JsonResponse
 case class Error(cause: Option[String] = None) extends JsonResponse
 
 case class ServerContextEncoder(ipAddress: String, port: Int) extends JsonResponse
+case class MatchesSetEncoder(set: Set[MatchRef]) extends JsonResponse
 
 object ResponseMessage {
   implicit val encodeEvent: Encoder[JsonResponse] = Encoder.instance {
@@ -26,6 +28,7 @@ object ResponseMessage {
     case message @ Message(_) => message.asJson
     case error @ Error(_) => error.asJson
     case context@ServerContextEncoder(_, _) => context.asJson
+    case set @ MatchesSetEncoder(_) => set.asJson
   }
 
   implicit val decodeEvent: Decoder[JsonResponse] =
@@ -33,6 +36,7 @@ object ResponseMessage {
       Decoder[Game].widen,
       Decoder[Message].widen,
       Decoder[Error].widen,
-      Decoder[ServerContextEncoder].widen
+      Decoder[ServerContextEncoder].widen,
+      Decoder[MatchesSetEncoder].widen
     ).reduceLeft(_ or _)
 }
