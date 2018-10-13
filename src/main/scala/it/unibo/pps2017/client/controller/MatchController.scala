4bacf5fcf76084d575a280ebf0b3621bd8294472
Tirocigno <fede.naldini1@gmail.com>
2018-10-13 15:18:11 +0200

    Added controller traits.

---
+++ src/main/scala/it/unibo/pps2017/client/controller/MatchController.scala
@@ -0,0 +1,8 @@
+
+package it.unibo.pps2017.client.controller
+
+/**
+  * MatchController is the mediator between the playerActor and the GUI.
+  */
+//TODO Integrate this with Brasini work.
+trait MatchController extends ActorController
