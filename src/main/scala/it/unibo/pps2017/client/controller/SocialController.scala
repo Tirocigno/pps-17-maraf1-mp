6bf7a39e9d6e72b9dc76fa2655ddd2675800b55b
Tirocigno <fede.naldini1@gmail.com>
2018-10-13 15:18:11 +0200

    Added controller traits.

---
+++ src/main/scala/it/unibo/pps2017/client/controller/SocialController.scala
@@ -0,0 +1,10 @@
+
+package it.unibo.pps2017.client.controller
+
+/**
+  * This trait is a mediator between the actor that handle the social
+  * function of the system and the GUI.
+  *
+  */
+//TODO DEVELOP GUI INTERACTION.
+trait SocialController extends ActorController
