793234956a6087be3fc27a1e32511a3c0502f106
Nicholas <nicholas.brasini@studio.unibo.it>
2018-10-17 11:11:14 +0200

    Revisited scala doc #2.

--- src/main/scala/it/unibo/pps2017/client/controller/actors/playeractor/GameController.scala
+++ src/main/scala/it/unibo/pps2017/client/controller/actors/playeractor/GameController.scala
@@ -21,8 +21,7 @@ class GameController extends ActorController {
   /**
     * Method to send to GUI ten cards of the user.
     *
-    * @param cards
-    * List of ten cards.
+    * @param cards List of ten cards.
     */
   def sendCardsFirstPlayer(cards: List[String]): Unit = {
     playGameController.getCardsFirstPlayer(cards.asJava)
@@ -31,8 +30,7 @@ class GameController extends ActorController {
   /**
     * Method to send to GUI briscola chosen from other player.
     *
-    * @param briscola
-    * Briscola chosen.
+    * @param briscola Briscola chosen.
     */
   def sendBriscolaChosen(briscola: String): Unit = {
     playGameController.getBriscolaChosen(briscola)
@@ -41,10 +39,8 @@ class GameController extends ActorController {
   /**
     * Method to send to GUI command chosen from other player.
     *
-    * @param player
-    * Player who clicks the command.
-    * @param command
-    * Command clicked by other player.
+    * @param player Player who clicks the command.
+    * @param command Command clicked by other player.
     */
   def sendCommand(player: String, command: String): Unit = {
     playGameController.getCommand(player, command)
@@ -53,10 +49,8 @@ class GameController extends ActorController {
   /**
     * Method to send to GUI path of played card from other player.
     *
-    * @param path
-    * Played card's path.
-    * @param player
-    * Player who played card.
+    * @param path Played card's path.
+    * @param player Player who played card.
     */
   def showOtherPlayersPlayedCard(path: String, player: String): Unit = {
     playGameController.showOtherPlayersPlayedCard(player, path)
@@ -65,16 +59,11 @@ class GameController extends ActorController {
   /**
     * Method to verify if player is a winner of this turn or not.
     *
-    * @param user
-    * Current player.
-    * @param winner1
-    * First of two winners of this turn.
-    * @param winner2
-    * Second of two winners of this turn.
-    * @param score1
-    * Aggregated score of first team.
-    * @param score2
-    * Aggregated score of second team.
+    * @param user Current player.
+    * @param winner1 First of two winners of this turn.
+    * @param winner2 Second of two winners of this turn.
+    * @param score1 Aggregated score of first team.
+    * @param score2 Aggregated score of second team.
     */
   def cleanFieldEndTotalTurn(user: String, winner1: String, winner2: String, score1: Int, score2: Int): Unit = {
 
@@ -91,16 +80,11 @@ class GameController extends ActorController {
   /**
     * Method to verify if player is a winner of match.
     *
-    * @param user
-    * Current player.
-    * @param winner1
-    * First of two winners of match.
-    * @param winner2
-    * Second of two winners of match.
-    * @param score1
-    * Final score of first team.
-    * @param score2
-    * Final score of second team.
+    * @param user Current player.
+    * @param winner1 First of two winners of match.
+    * @param winner2 Second of two winners of match.
+    * @param score1 Final score of first team.
+    * @param score2 Final score of second team.
     */
   def cleanFieldEndMatch(user: String, winner1: String, winner2: String, score1: Int, score2: Int): Unit = {
 
@@ -127,12 +111,9 @@ class GameController extends ActorController {
   /**
     * Method to send to GUI the player that now will be plays a card.
     *
-    * @param player
-    * Player that will be plays a card.
-    * @param partialTurnIsEnded
-    * Boolean to know if turn is ended.
-    * @param isFirstPlayer
-    * Boolean to know if the player is the first of the turn (for show or hide commands)
+    * @param player Player that will be plays a card.
+    * @param partialTurnIsEnded Boolean to know if turn is ended.
+    * @param isFirstPlayer Boolean to know if the player is the first of the turn (for show or hide commands)
     */
   def setCurrentPlayer(player: String, partialTurnIsEnded: Boolean, isFirstPlayer: Boolean): Unit = {
     playGameController.setCurrentPlayer(player, partialTurnIsEnded, isFirstPlayer)
@@ -148,8 +129,7 @@ class GameController extends ActorController {
   /**
     * Method to send to GUI the current value of timer.
     *
-    * @param timer
-    * Value of timer.
+    * @param timer Value of timer.
     */
   def setTimer(timer: Int): Unit = {
     playGameController.setTimer(timer)
@@ -158,21 +138,25 @@ class GameController extends ActorController {
   /**
     * Method to send to PlayerActorClient the clicked command from actual player.
     *
-    * @param command
-    * Clicked command from actual player.
+    * @param command Clicked command from actual player.
     */
   def setCommandFromPlayer(command: String): Unit = {
     currentActorRef ! ClickedCommandActualPlayer(command)
   }
 
+  /**
+    * Method to access the inner value of the Option, unless there is None. If None, we get the argument back.
+    *
+    * @param actorRef actor's ref.
+    * @return Inner value of the Option.
+    */
   def getOrThrow(actorRef: Option[ActorRef]): ActorRef =
     actorRef.getOrElse(throw new NoSuchElementException(noActorFoundMessage))
 
   /**
     * Method to send to PlayerActorClient to inform it of played card.
     *
-    * @param cardIndex
-    * Index of played card.
+    * @param cardIndex Index of played card.
     */
   def setPlayedCard(cardIndex: Int): Unit = {
     currentActorRef ! ClickedCardActualPlayer(cardIndex)
@@ -181,8 +165,7 @@ class GameController extends ActorController {
   /**
     * Method to send to PlayerActorClient his username.
     *
-    * @param playerUsername
-    * Player's username
+    * @param playerUsername Player's username.
     */
   def setUsernamePlayer(playerUsername: String): Unit = {
     currentActorRef ! SetUsernamePlayer(playerUsername)
@@ -191,8 +174,7 @@ class GameController extends ActorController {
   /**
     * Method to send to PlayerActorClient id of match.
     *
-    * @param id
-    * Match's id
+    * @param id Match's id.
     */
   def joinPlayerToMatch(id: String): Unit = {
     currentActorRef ! IdChannelPublishSubscribe(id)
@@ -201,10 +183,8 @@ class GameController extends ActorController {
   /**
     * Method to create a new PlayerActorClient.
     *
-    * @param actorId
-    * Id of PlayerActorClient.
-    * @param actorSystem
-    * System.
+    * @param actorId Id of PlayerActorClient.
+    * @param actorSystem System.
     */
   override def createActor(actorId: String, actorSystem: ActorSystem): Unit = {
     currentActorRef = actorSystem.actorOf(Props(new PlayerActorClient(this, actorId)))
@@ -213,8 +193,7 @@ class GameController extends ActorController {
   /**
     * Method to know if is the turn of actual player.
     *
-    * @return
-    * myTurn
+    * @return myTurn
     */
   def isMyTurn: Boolean = {
     myTurn
@@ -223,8 +202,7 @@ class GameController extends ActorController {
   /**
     * Method to set the turn.
     *
-    * @param turn
-    * Boolean turn.
+    * @param turn Boolean turn.
     */
   def setMyTurn(turn: Boolean): Unit = {
     myTurn = turn
@@ -233,8 +211,7 @@ class GameController extends ActorController {
   /**
     * Method to send to GUI to understand if clicked card is ok or not.
     *
-    * @param cardOK
-    * Boolean to know if clicked card is ok or not.
+    * @param cardOK Boolean to know if clicked card is ok or not.
     */
   def setCardOK(cardOK: Boolean): Unit =
     if (cardOK) playGameController.showPlayedCardOk() else playGameController.showPlayedCardError()
@@ -242,8 +219,7 @@ class GameController extends ActorController {
   /**
     * Method to send to GUI four players of the match.
     *
-    * @param playersList
-    * Players' list of match.
+    * @param playersList layers' list of match.
     */
   def sendPlayersList(playersList: List[String]): Unit = {
     playGameController.setPlayersList(playersList.asJava)
@@ -252,8 +228,7 @@ class GameController extends ActorController {
   /**
     * Method to send to PlayerActorClient to inform of briscola chosen.
     *
-    * @param briscola
-    * Briscola chosen from current player.
+    * @param briscola Briscola chosen from current player.
     */
   def selectedBriscola(briscola: String): Unit = briscola match {
     case Sword.asString => currentActorRef ! BriscolaChosen(Sword)
@@ -266,8 +241,7 @@ class GameController extends ActorController {
   /**
     * Method to set variable amIWinner.
     *
-    * @param winner
-    * Boolean variable.
+    * @param winner Boolean variable.
     */
   def setWinner(winner: Boolean): Unit = {
     this.amIWinner = winner
@@ -276,8 +250,7 @@ class GameController extends ActorController {
   /**
     * Method to know if the current player is the winner.
     *
-    * @return
-    * getWinner Boolean variable.
+    * @return getWinner Boolean variable.
     */
   def getWinner: Boolean = {
     this.amIWinner
@@ -286,8 +259,7 @@ class GameController extends ActorController {
   /**
     * Method called from PlayerActorClient to update GUI.
     *
-    * @param message
-    * Type of message from PlayerActorClient.
+    * @param message Type of message from PlayerActorClient.
     */
   override def updateGUI(message: ActorMessage): Unit = message match {
     case PlayersRef(playersList) => sendPlayersList(playersList.toList)
