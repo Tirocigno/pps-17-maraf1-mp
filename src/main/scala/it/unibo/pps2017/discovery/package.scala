55af21652cb0621c5fa695bdfb0cc9f54997688c
Tirocigno <fede.naldini1@gmail.com>
2018-10-17 17:19:28 +0200

    Converted IP and Port from source to form parameters

--- src/main/scala/it/unibo/pps2017/discovery/package.scala
+++ src/main/scala/it/unibo/pps2017/discovery/package.scala
@@ -3,7 +3,7 @@ package it.unibo.pps2017
 
 import io.vertx.scala.ext.web.RoutingContext
 import it.unibo.pps2017.commons.remote.RestUtils.{MatchRef, ServerContext}
-import it.unibo.pps2017.discovery.restAPI.DiscoveryAPI.{DiscoveryAPI, GetServerAPI}
+import it.unibo.pps2017.discovery.restAPI.DiscoveryAPI.GetServerAPI
 import it.unibo.pps2017.server.model.MatchesSetEncoder
 
 import scala.language.implicitConversions
@@ -12,9 +12,8 @@ package object discovery {
 
   implicit def retreiveContextFromRequest(router: RoutingContext): ServerContext = {
     val request = router.request()
-    val discoveryAPI: DiscoveryAPI = GetServerAPI
-    ServerContext(request.getFormAttribute(discoveryAPI.getIpKey).get, Integer.valueOf(request.getFormAttribute(discoveryAPI
-      .getPortKey).get))
+    ServerContext(request.getFormAttribute(GetServerAPI.getIpKey).get,
+      Integer.valueOf(request.getFormAttribute(GetServerAPI.getPortKey).get))
   }
 
   implicit def matchesSetToJson(matchesSet:Set[MatchRef]):MatchesSetEncoder =
