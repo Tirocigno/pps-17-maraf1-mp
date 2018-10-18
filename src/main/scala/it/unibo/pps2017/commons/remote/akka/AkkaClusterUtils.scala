1bd10d36b0108a5b0e57805c77a46af3c6966527
Tirocigno <fede.naldini1@gmail.com>
2018-10-18 12:04:38 +0200

    Defined new method and improved scaladoc

--- src/main/scala/it/unibo/pps2017/commons/remote/akka/AkkaClusterUtils.scala
+++ src/main/scala/it/unibo/pps2017/commons/remote/akka/AkkaClusterUtils.scala
@@ -5,23 +5,39 @@ import com.typesafe.config.ConfigFactory
 
 object AkkaClusterUtils {
 
-  def startSeedCluster: Unit = {
+  /**
+    * Standard name for actorsystems, must be the same on all the actorsystem.
+    */
+  val STANDARD_SYSTEM_NAME = "maraph-1System"
+
+  /**
+    * Create the two nodes for the cluster.
+    */
+  def startSeedCluster(): Unit = {
     val ports = Set("2551", "2552")
-    ports foreach {
-      startJoiningActorSystem("DiscoverySystem", _)
-    }
+    ports foreach startJoiningActorSystem
   }
 
-  def startJoiningActorSystem(systemName: String, port: String): ActorSystem = {
+  /**
+    * Create a new node which will join the cluster.
+    *
+    * @param port the port on which the cluster will be run.
+    * @return an actorsystem joined to the cluster.
+    */
+  def startJoiningActorSystem(port: String): ActorSystem = {
     val config = ConfigFactory.parseString(
       s"""
         akka.remote.netty.tcp.port=$port
         akka.remote.artery.canonical.port=$port
         """).withFallback(ConfigFactory.load())
-    println("Config loaded")
-    val actorsystem = ActorSystem(systemName, config)
-    println("ActorSystem created")
-    actorsystem
+    ActorSystem(STANDARD_SYSTEM_NAME, config)
   }
+
+  /**
+    * Create a new actorsystem, running on a random port.
+    *
+    * @return an actorsystem which will eventually join the cluster.
+    */
+  def startJoiningActorSystemOnRandomPort(): ActorSystem = startJoiningActorSystem("0")
 
 }
