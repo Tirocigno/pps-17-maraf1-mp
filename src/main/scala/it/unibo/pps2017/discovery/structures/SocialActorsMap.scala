a1cb6ac99895b8eb2f9d397edcf2437d87a528dc
Tirocigno <fede.naldini1@gmail.com>
2018-10-23 12:37:49 +0200

    Defined trait methods.

--- src/main/scala/it/unibo/pps2017/discovery/structures/SocialActorsMap.scala
+++ src/main/scala/it/unibo/pps2017/discovery/structures/SocialActorsMap.scala
@@ -1,8 +1,30 @@
 package it.unibo.pps2017.discovery.structures
 
+import akka.actor.ActorRef
+
 /**
   * This structure will handle a list of actor refs which correspond to the online players logged in the system.
   */
 trait SocialActorsMap {
+
+  /**
+    * Add a new user on the online player list.
+    *
+    * @param userID         the id of the user.
+    * @param socialActorRef the user Social Actor Ref.
+    */
+  def registerUser(userID: String, socialActorRef: ActorRef)
+
+  /**
+    * Remove a user from the online players list
+    *
+    * @param userID the id of the player to remove.
+    */
+  def unregisterUser(userID: String)
+
+  /**
+    * Return the map of current online players.
+    */
+  def getCurrentOnlinePlayerMap: Map[String, ActorRef]
 
 }
