b542a9bcbde522b5425f5bed6c437da09fb5f3f2
Tirocigno <fede.naldini1@gmail.com>
2018-10-23 12:30:14 +0200

    Created SocialActorMap

---
+++ src/main/scala/it/unibo/pps2017/discovery/structures/SocialActorsMap.scala
@@ -0,0 +1,8 @@
+package it.unibo.pps2017.discovery.structures
+
+/**
+  * This structure will handle a list of actor refs which correspond to the online players logged in the system.
+  */
+trait SocialActorsMap {
+
+}
