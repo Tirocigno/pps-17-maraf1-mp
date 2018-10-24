1bc4144442590704cf423cfd7758525659f7c837
jacopo <jacopo.riciputi@gmail.com>
2018-10-24 15:39:31 +0200

    Bug fixing, test moved from Cornichon to ScalaTest

--- src/test/scala/it/unibo/pps2017/server/controller/UserDispatcherTest.scala
+++ src/test/scala/it/unibo/pps2017/server/controller/UserDispatcherTest.scala
@@ -1,6 +1,6 @@
 package it.unibo.pps2017.server.controller
 
-import java.lang.Thread.sleep
+import java.lang.Thread._
 
 import akka.actor.ActorSystem
 import com.github.agourlay.cornichon.CornichonFeature
@@ -10,7 +10,7 @@ import it.unibo.pps2017.server.controller.DispatcherTest.PRIMARY_URL
 import it.unibo.pps2017.server.controller.LobbyFoundTest.WAIT_TIMEOUT
 import it.unibo.pps2017.server.controller.UserDispatcherTest.{friendForTest, usernameForTest}
 import it.unibo.pps2017.server.model.ResponseStatus.{EXCEPTION_CODE, OK_CODE}
-import it.unibo.pps2017.server.model.{Message, PostRequest}
+import it.unibo.pps2017.server.model.{GetRequest, Message, PostRequest, UserFriends}
 import org.json4s.DefaultFormats
 import org.json4s.jackson.Serialization.read
 import org.scalatest.{BeforeAndAfterEach, FunSuite}
@@ -108,6 +108,18 @@ class AddFriendshipTest extends FunSuite with BeforeAndAfterEach {
     sleep(WAIT_TIMEOUT)
   }
 
+  test("GetFriends") {
+    GetRequest(Dispatcher.HOST, "/user/getFriends/" + usernameForTest, onSuccess => {
+      val userWithFriends = read[UserFriends](onSuccess.get)
+
+      assert(userWithFriends.friends.head == friendForTest)
+    }, cause => {
+      assert(false)
+    }, None, Some(Dispatcher.PORT))
+
+    sleep(WAIT_TIMEOUT)
+
+  }
 
   test("RemoveFriendship") {
     PostRequest(Dispatcher.HOST, "/user/removeFriend/" + usernameForTest, onSuccess => {
@@ -120,55 +132,34 @@ class AddFriendshipTest extends FunSuite with BeforeAndAfterEach {
 
     sleep(WAIT_TIMEOUT)
   }
+
+  test("DeleteUser") {
+    PostRequest(Dispatcher.HOST, "/user/removeUser/" + usernameForTest, onSuccess => {
+      val msg = read[Message](onSuccess.get)
+
+      assert(msg.message == s"User $usernameForTest deleted correctly!")
+    }, _ => {
+      assert(false)
+    }, None, Some(Dispatcher.PORT))
+
+    sleep(WAIT_TIMEOUT)
+  }
+
+  test("DeleteUserFriend") {
+    PostRequest(Dispatcher.HOST, "/user/removeUser/" + friendForTest, onSuccess => {
+      val msg = read[Message](onSuccess.get)
+
+      assert(msg.message == s"User $friendForTest deleted correctly!")
+    }, _ => {
+      assert(false)
+    }, None, Some(Dispatcher.PORT))
+
+    sleep(WAIT_TIMEOUT)
+  }
+
 }
 
 
-class DeleteUserTest extends CornichonFeature {
-  implicit val formats: DefaultFormats.type = DefaultFormats
-
-  val verticle = Dispatcher(
-    ActorSystem("Test"))
-
-  var verticleId: Option[String] = None
-
-
-  beforeFeature {
-    Vertx.vertx().deployVerticleFuture(verticle).onComplete {
-      case Success(res) => verticleId = Some(res)
-      case Failure(cause) => println("Error on verticle deployment, cause: " + cause)
-    }
-  }
-
-  afterFeature {
-    verticleId match {
-      case Some(id) => Vertx.vertx().undeploy(id)
-      case None => println("No verticle to close!")
-    }
-  }
-
-  override def feature: FeatureDef = Feature("CleanUsersForTest") {
-
-
-    Scenario("GetFriends") {
-      When I get(PRIMARY_URL + "/user/getFriends/" + usernameForTest)
-      Then assert status.is(OK_CODE)
-      And assert body.path("friends").isPresent
-    }
-
-
-    Scenario("DeleteUser") {
-      When I post(PRIMARY_URL + "/user/removeUser/" + usernameForTest)
-      Then assert status.is(OK_CODE)
-      And assert body.path("message").is(s"User $usernameForTest deleted correctly!")
-    }
-
-    Scenario("DeleteUserFriend") {
-      When I post(PRIMARY_URL + "/user/removeUser/" + friendForTest)
-      Then assert status.is(OK_CODE)
-      And assert body.path("message").is(s"User $friendForTest deleted correctly!")
-    }
-  }
-}
 
 object UserDispatcherTest {
   val usernameForTest = "ThisUserIsUsedForTest"
