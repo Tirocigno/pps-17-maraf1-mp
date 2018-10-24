35f0292566929dab3a0095af46bd5ddd635287e3
Tirocigno <fede.naldini1@gmail.com>
2018-10-24 16:25:21 +0200

    Created basic implementation for SocialRestWebClient

---
+++ src/main/scala/it/unibo/pps2017/client/model/remote/SocialRestWebClient.scala
@@ -0,0 +1,35 @@
+
+package it.unibo.pps2017.client.model.remote
+
+import it.unibo.pps2017.client.controller.SocialController
+import it.unibo.pps2017.commons.remote.API
+import it.unibo.pps2017.commons.remote.RestUtils.{ServerContext, formats}
+import it.unibo.pps2017.discovery.restAPI.DiscoveryAPI.{GetAllOnlinePlayersAPI, RegisterSocialIDAPI, UnregisterSocialIDAPI}
+import it.unibo.pps2017.server.model.OnlinePlayersMapEncoder
+import org.json4s.jackson.Serialization.read
+
+/**
+  * This class takes care of all the rest call executed by the social structure of the architecture.
+  */
+class SocialRestWebClient(val socialController: SocialController, val discoveryContext: ServerContext)
+  extends AbstractRestWebClient(discoveryContext) {
+
+  override def executeAPICall(api: API.RestAPI, paramMap: Option[Map[String, Any]]): Unit = api match {
+    case RegisterSocialIDAPI => invokeAPI(api, paramMap, registerAndUnregisterSocialIDCallBack)
+    case UnregisterSocialIDAPI => invokeAPI(api, paramMap, registerAndUnregisterSocialIDCallBack)
+    case GetAllOnlinePlayersAPI => invokeAPI(api, paramMap, getAllOnlinePlayersCallback)
+  }
+
+  private def registerAndUnregisterSocialIDCallBack(responseBody: Option[String]): Unit =
+    socialController.notifyCallResultToGUI(responseBody)
+
+  private def getAllOnlinePlayersCallback(responseBody: Option[String]): Unit = {
+    val playerMap = read[OnlinePlayersMapEncoder](responseBody.get).map
+    socialController.setAndDisplayOnlinePlayerList(playerMap)
+  }
+}
+
+object SocialRestWebClient {
+  def apply(socialController: SocialController, discoveryContext: ServerContext): RestWebClient =
+    new SocialRestWebClient(socialController, discoveryContext)
+}
