4968edf981194a6034d891e152b3e2fe5cf02873
Tirocigno <fede.naldini1@gmail.com>
2018-10-24 16:20:47 +0200

    Added more methods on SocialController trait

--- src/main/scala/it/unibo/pps2017/client/controller/SocialController.scala
+++ src/main/scala/it/unibo/pps2017/client/controller/SocialController.scala
@@ -1,10 +1,22 @@
 
 package it.unibo.pps2017.client.controller
 
+import it.unibo.pps2017.discovery.structures.SocialActorsMap.SocialMap
+
 /**
   * This trait is a mediator between the actor that handle the social
   * function of the system and the GUI.
   *
   */
 //TODO DEVELOP GUI INTERACTION.
-trait SocialController extends ActorController
+trait SocialController extends ActorController {
+
+  /**
+    * Notify a API callback response to the GUI
+    *
+    * @param message the body of the response.
+    */
+  def notifyCallResultToGUI(message: Option[String]): Unit
+
+  def setAndDisplayOnlinePlayerList(playerList: SocialMap): Unit
+}
