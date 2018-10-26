7c2d0e31a8c3e05de30ed683b21295e255d0e4ea
Tirocigno <fede.naldini1@gmail.com>
2018-10-26 20:43:31 +0200

    Created SocialResponse trait

---
+++ src/main/scala/it/unibo/pps2017/commons/remote/social/SocialResponse.scala
@@ -0,0 +1,16 @@
+package it.unibo.pps2017.commons.remote.social
+
+/**
+  * Trait to model the possible response for a message.
+  */
+trait SocialResponse
+
+object SocialResponse {
+
+  case object PositiveResponse extends SocialResponse
+
+  case object NegativeResponse extends SocialResponse
+
+}
+
+
