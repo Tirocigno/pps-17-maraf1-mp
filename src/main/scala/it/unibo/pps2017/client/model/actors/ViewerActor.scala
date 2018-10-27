967e267553875f1d5dbc9a15eb151ee65abf8171
Nicholas <nicholas.brasini@studio.unibo.it>
2018-10-27 10:59:48 +0200

    Added case RecapActualSituation

--- src/main/scala/it/unibo/pps2017/client/model/actors/ViewerActor.scala
+++ src/main/scala/it/unibo/pps2017/client/model/actors/ViewerActor.scala
@@ -1,6 +1,30 @@
 
 package it.unibo.pps2017.client.model.actors
 
-class ViewerActor {
+import it.unibo.pps2017.client.controller.actors.playeractor.GameController
+import it.unibo.pps2017.client.model.actors.playeractor.ClientMessages._
+
+class ViewerActor(override val controller: GameController, username: String) extends ClientGameActor {
+
+  var actorPlayer: ClientGameActor = this
+  var user: String = username
+
+  def receive: PartialFunction[Any, Unit] = {
+
+    case RecapActualSituation(playersList, cards, seed, player) => {
+      controller.updateGUI(PlayersRef(playersList))
+      controller.updateGUI(DistributedCard(cards, username))
+      controller.updateGUI(NotifyBriscolaChosen(seed))
+      controller.updateGUI(Turn(player, true, false))
+    }
+
+
+  }
+
+
+  override
+  def getUsername: String = {
+    user
+  }
 
 }
