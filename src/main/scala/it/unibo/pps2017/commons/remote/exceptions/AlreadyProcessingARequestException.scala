56f4401423f92c15f733f90bdee355df121746c9
Tirocigno <fede.naldini1@gmail.com>
2018-10-27 10:56:40 +0200

    Added new exception

---
+++ src/main/scala/it/unibo/pps2017/commons/remote/exceptions/AlreadyProcessingARequestException.scala
@@ -0,0 +1,7 @@
+
+package it.unibo.pps2017.commons.remote.exceptions
+
+/**
+  * Exception to throw when a request is already handled and a new one is registered.
+  */
+class AlreadyProcessingARequestException extends Exception
