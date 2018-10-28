7b2c5da39a4a9195d22a357f629c0bde2abca0ca
Tirocigno <fede.naldini1@gmail.com>
2018-10-28 10:21:00 +0100

    Refactored code inside RequestHandler

--- src/main/scala/it/unibo/pps2017/client/model/actors/socialactor/socialstructures/RequestHandler.scala
+++ src/main/scala/it/unibo/pps2017/client/model/actors/socialactor/socialstructures/RequestHandler.scala
@@ -5,9 +5,9 @@ import it.unibo.pps2017.client.model.actors.socialactor.socialmessages.SocialMes
 import it.unibo.pps2017.commons.remote.exceptions.AlreadyProcessingARequestException
 import it.unibo.pps2017.commons.remote.social.PartyPlayer.{FoePlayer, PartnerPlayer}
 import it.unibo.pps2017.commons.remote.social.PartyRole.{Foe, Partner}
-import it.unibo.pps2017.commons.remote.social.SocialResponse
 import it.unibo.pps2017.commons.remote.social.SocialResponse.{NegativeResponse, PositiveResponse}
 import it.unibo.pps2017.commons.remote.social.SocialUtils.PlayerReference
+import it.unibo.pps2017.commons.remote.social.{PartyRole, SocialResponse}
 
 /**
   * A class for register the status of a received request.
@@ -50,6 +50,9 @@ object RequestHandler {
       case InvitePlayerRequestMessage(_, _) => checkRequestAndExecute(requestMessage)(inviteHandler)
     }
 
+    override def respondToRequest(socialResponse: SocialResponse): Unit =
+      checkMessageAndElaborateResponse(socialResponse)
+
     /**
       * Check if a request is already served, if not register a new one
       *
@@ -87,19 +90,67 @@ object RequestHandler {
       }
     }
 
-    override def respondToRequest(socialResponse: SocialResponse): Unit = currentMessage.get match {
-      case AddFriendRequestMessage(sender) => sender.playerRef ! AddFriendResponseMessage(socialResponse); resetRequestHandler()
-      case InvitePlayerRequestMessage(sender, role) =>
-        socialResponse match {
-          case NegativeResponse => sender.playerRef ! InvitePlayerResponseMessage(socialResponse, None, None)
-          case PositiveResponse => role match {
-            case Partner => sender.playerRef ! InvitePlayerResponseMessage(socialResponse,
-              Some(PartnerPlayer(currentPlayerRef)), None)
-            case Foe => sender.playerRef ! InvitePlayerResponseMessage(socialResponse,
-              Some(FoePlayer(currentPlayerRef)), currentParty.getPartner)
-          }
-        }
+    /**
+      * Check if a message is present and elaborate its message response, otherwise throws a NoSuchElement exception.
+      *
+      * @param socialResponse the response sent by the user.
+      */
+    private def checkMessageAndElaborateResponse(socialResponse: SocialResponse): Unit = currentMessage match {
+      case Some(message) => elaborateResponse(message, socialResponse)
+      case None => throw new NoSuchElementException()
     }
+
+    /**
+      * Elaborate a response based on the message and the response.
+      *
+      * @param socialMessage  the message to respond to.
+      * @param socialResponse the response, could be positive or negative.
+      */
+    private def elaborateResponse(socialMessage: RequestMessage, socialResponse: SocialResponse): Unit =
+      socialMessage match {
+        case AddFriendRequestMessage(sender) => generateFriendResponse(socialResponse, sender); resetRequestHandler()
+        case InvitePlayerRequestMessage(sender, role) => generateInviteResponse(socialResponse, sender, role); resetRequestHandler()
+      }
+
+    /**
+      * Generate a response for a FriendRequest message and sends it to request sender,
+      *
+      * @param socialResponse the response, could be positive or negative.
+      * @param sender         the sender of request.
+      */
+    private def generateFriendResponse(socialResponse: SocialResponse, sender: PlayerReference): Unit =
+      sender.playerRef ! AddFriendResponseMessage(socialResponse)
+
+    /**
+      * Generate a response for a InviteRequest message and it sends back to sender.
+      *
+      * @param socialResponse  the response, could be positive or negative.
+      * @param playerReference the sender references.
+      * @param role            the role on which the request want the player to play as.
+      */
+    private def generateInviteResponse(socialResponse: SocialResponse,
+                                       playerReference: PlayerReference, role: PartyRole): Unit = socialResponse match {
+      case NegativeResponse => playerReference.playerRef ! InvitePlayerResponseMessage(socialResponse, None, None)
+        resetRequestHandler()
+      case PositiveResponse => playerReference.playerRef ! generateInviteResponseMessage(socialResponse, role)
+        resetRequestHandler()
+    }
+
+    /**
+      * Generate the response message for a invite request.
+      *
+      * @param socialResponse the response, could be positive or negative.
+      * @param role           role the role on which the request want the player to play as.
+      * @return an InvitePlayerResponse message.
+      */
+    private def generateInviteResponseMessage(socialResponse: SocialResponse, role: PartyRole): SocialMessage =
+      role match {
+        case Partner => InvitePlayerResponseMessage(socialResponse, Some(PartnerPlayer(currentPlayerRef)), None)
+        case Foe => InvitePlayerResponseMessage(socialResponse,
+          Some(FoePlayer(currentPlayerRef)), currentParty.getPartner)
+        case _ => throw new IllegalArgumentException()
+      }
+
 
     /**
       * Reset internal request fields after each response.
