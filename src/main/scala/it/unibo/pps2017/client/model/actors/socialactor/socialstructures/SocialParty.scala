c0b0921c93f28ee417c74819a009619a528ecace
Tirocigno <fede.naldini1@gmail.com>
2018-10-28 11:10:11 +0100

    Implemented socialParty methods

--- src/main/scala/it/unibo/pps2017/client/model/actors/socialactor/socialstructures/SocialParty.scala
+++ src/main/scala/it/unibo/pps2017/client/model/actors/socialactor/socialstructures/SocialParty.scala
@@ -1,7 +1,9 @@
 
 package it.unibo.pps2017.client.model.actors.socialactor.socialstructures
 
+import it.unibo.pps2017.client.model.actors.socialactor.socialmessages.SocialMessages.NotifyGameIDMessage
 import it.unibo.pps2017.commons.remote.social.PartyRole
+import it.unibo.pps2017.commons.remote.social.PartyRole.{Leader, Partner}
 import it.unibo.pps2017.commons.remote.social.SocialUtils.PlayerReference
 
 /**
@@ -9,18 +11,79 @@ import it.unibo.pps2017.commons.remote.social.SocialUtils.PlayerReference
   */
 trait SocialParty {
 
+  /**
+    * Check if the player is the leader of the party(then it can send/accept invite request) or not.
+    *
+    * @return true if the player is the leader of the party, false otherwise.
+    */
   def isLeader: Boolean
 
-  def setPlayerInParty(role: PartyRole, playerContext: PlayerReference)
+  /**
+    * Set a specific player with a specific role inside the party.
+    *
+    * @param role          the role on which the player will be set.
+    * @param playerContext the player ID and actorRef.
+    */
+  def setPlayerInParty(role: PartyRole, playerContext: PlayerReference): Unit
 
-  def notifyGameIDToAllPlayers(gameID: String)
+  /**
+    * Notify to all players inside the party that a game has started.
+    *
+    * @param gameID the id of the game
+    */
+  def notifyGameIDToAllPlayers(gameID: String): Unit
 
-  def resetParty()
+  /**
+    * Reset the party, setting current player as leader of the party.
+    */
+  def resetParty(): Unit
 
+  /**
+    * Get all the players inside the party.
+    *
+    * @return a Map containing every player registered inside the party.
+    */
   def getAllPlayers: Map[PartyRole, PlayerReference]
 
-  def markCurrentPlayerAs(partyRole: PartyRole)
+  /**
+    * Remove the leadership from the current player and set a new role for him.
+    *
+    * @param partyRole the role current player will play as.
+    */
+  def markCurrentPlayerAs(partyRole: PartyRole): Unit
 
+  /**
+    * Get the reference of the player's partner, if present.
+    *
+    * @return a optional containing the partner references.
+    */
   def getPartner: Option[PlayerReference]
+
+}
+
+object SocialParty {
+
+  private class SocialPartyImpl(val currentReference: PlayerReference) extends SocialParty {
+    var partyMap: Map[PartyRole, PlayerReference] = Map(Leader -> currentReference)
+
+    override def isLeader: Boolean = partyMap.keys.exists(_.equals(Leader))
+
+    override def resetParty(): Unit = partyMap = Map(Leader -> currentReference)
+
+    override def getAllPlayers: Map[PartyRole, PlayerReference] = partyMap
+
+    override def setPlayerInParty(role: PartyRole, playerContext: PlayerReference): Unit =
+      partyMap += (role -> playerContext)
+
+    override def markCurrentPlayerAs(partyRole: PartyRole): Unit = {
+      partyMap -= Leader
+      partyMap += (partyRole -> currentReference)
+    }
+
+    override def getPartner: Option[PlayerReference] = partyMap.get(Partner)
+
+    override def notifyGameIDToAllPlayers(gameID: String): Unit =
+      partyMap.values.filter(!_.equals(currentReference)).map(_.playerRef).foreach(_ ! NotifyGameIDMessage(gameID))
+  }
 
 }
