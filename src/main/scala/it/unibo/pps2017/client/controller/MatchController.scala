d64ec12f04f540e346d1d409f378d8e94f58b30c
Tirocigno <fede.naldini1@gmail.com>
2018-10-29 18:11:55 +0100

    Adapted GameGUI to setGUI method

--- src/main/scala/it/unibo/pps2017/client/controller/MatchController.scala
+++ src/main/scala/it/unibo/pps2017/client/controller/MatchController.scala
@@ -1,8 +1,14 @@
 
 package it.unibo.pps2017.client.controller
 
+import it.unibo.pps2017.client.view.game.GameGUIController
+
 /**
   * MatchController is the mediator between the playerActor and the GUI.
   */
-//TODO Integrate this with Brasini work.
-trait MatchController extends ActorController
+trait MatchController extends ActorController {
+
+  override var currentGUI: GameGUIController = _
+
+  override def setGUI[A <: GameGUIController](gui: A): Unit = this.currentGUI = gui
+}
