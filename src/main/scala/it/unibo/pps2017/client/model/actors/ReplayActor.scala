17cf0fd4f6b5332d4bf42ee1cca4c611753b62e0
Nicholas <nicholas.brasini@studio.unibo.it>
2018-10-29 18:18:57 +0100

    Added managing of scores and revisited foreach for turn with counters.

--- src/main/scala/it/unibo/pps2017/client/model/actors/ReplayActor.scala
+++ src/main/scala/it/unibo/pps2017/client/model/actors/ReplayActor.scala
@@ -1,34 +1,46 @@
 
 package it.unibo.pps2017.client.model.actors
 
-import akka.actor.{ActorSystem, Props}
+import akka.actor.ActorSystem
 import it.unibo.pps2017.client.controller.actors.playeractor.GameController
 import it.unibo.pps2017.client.model.actors.ReplayActor.SendHeartbeat
 import it.unibo.pps2017.client.model.actors.playeractor.ClientMessages._
 import it.unibo.pps2017.core.deck.cards.Seed._
-import it.unibo.pps2017.demo.Swap
 import it.unibo.pps2017.server.model.Game
 
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.duration._
 
 object ReplayActor {
+
   case class SendHeartbeat()
+
 }
 
-//class ReplayActor() extends Actor{
+//class ReplayActor() extends Actor {
 
 class ReplayActor(override val controller: GameController, username: String, game: Game) extends ClientGameActor {
 
   var user: String = username
+
   import system.dispatcher
+
   val system = ActorSystem("ScheduledActors")
 
-  var i: Int = 0
+  var gameCounter: Int = 0
 
   var cardsListPlayer = new ListBuffer[String]()
   var playersList = new ListBuffer[String]()
   var briscolaChosen: Seed = _
+
+  /* due variabili per gestire il momento in cui devo recuperare le carte del primo utente */
+  var counterTurn = 1
+  var counterForPlayerHand = 0
+
+  /* le due variabili per il punteggio delle due squadre */
+  var team1Score = 0
+  var team2Score = 0
+
   final val FIRST_CARD: Int = 0
   final val LAST_CARD: Int = 10
   final val CARD_PATH: String = "cards/"
@@ -36,72 +48,79 @@ class ReplayActor(override val controller: GameController, username: String, gam
 
 
   override def preStart() {
-      system.scheduler.schedule(
-        initialDelay = 0 milliseconds,
-        interval = 3000 milliseconds,
-        receiver = self,
-        message = SendHeartbeat)
+    system.scheduler.schedule(
+      initialDelay = 0 milliseconds,
+      interval = 3000 milliseconds,
+      receiver = self,
+      message = SendHeartbeat)
   }
-
 
 
   def receive: PartialFunction[Any, Unit] = {
 
     case SendHeartbeat =>
-/*
-      val gameBackup = new GameBackupImpl("TEST")
-      val player1: String = "Nicholas"
-      val player1Hand: Set[Card] = Set(CardImpl(Coin, 4), CardImpl(Coin, 3), CardImpl(Sword, 4), CardImpl(Club, 9))
-      val player2: String = "Gjulio"
-      val player2Hand: Set[Card] = Set(CardImpl(Coin, 10), CardImpl(Coin, 3), CardImpl(Sword, 4), CardImpl(Club, 9))
-      val player3: String = "Federico"
-      val player3Hand: Set[Card] = Set(CardImpl(Coin, 3), CardImpl(Sword, 3), CardImpl(Sword, 4), CardImpl(Cup, 9))
-      val player4: String = "Jacopo"
-      val player4Hand: Set[Card] = Set(CardImpl(Cup, 7), CardImpl(Sword, 3), CardImpl(Sword, 4), CardImpl(Club, 9))
-      val playersInGame: Seq[String] = Seq(player1, player2, player3, player4)
-      val playersHand = Map(player1 -> player1Hand, player2 -> player2Hand, player3 -> player3Hand, player4 -> player4Hand)
-      val winners: Seq[String] = Seq(player1, player3)
-      val moves: Seq[Move] = Seq(Move(player2, "Coin4"),
-        Move(player3, "Coin4"),
-        Move(player4, "Cup4"),
-        Move(player1, "Coin3"))
-      val hand = Hand(moves, player4)
-      val gameSet: GameSet = GameSet(
-        playersHand.map({ case (k, v) => (k, v.map(card => card.cardSeed.asString + card.cardValue)) }),
-        Seq(hand), Sword.asString, 11, 0)
-      val expectedGame: Game = Game(playersInGame, Seq(gameSet), winners)
-*/
 
-      // IF PER I NOMI DEI QUATTRO GIOCATORI
-
-      if (i == 0) {
+      /**
+        * Situazione zero. Recupero gli username dei quattro player.
+        */
+      if (gameCounter == 0) {
         game.players.foreach(player => {
           playersList += player
         })
         controller.updateGUI(PlayersRef(playersList))
 
-        // IF PER COSTRUIRE LA LISTA DELLE CARTE DEL PRIMO GIOCATORE
-      } else if (i == 1) {
+        /**
+          * Situazione uno. Recupero le carte in mano al primo giocatore.
+           Il primo giro avro' counterForPlayerHand = 1 e counterTurn = 1, quindi prendo il primo turn.
+           Il secondo giro avro' inizialmente counterForPlayerHand = 1 (azzerato alla fine del ciclo precedente e incrementato
+           a 1 appena entrato) counterTurn = 2, quindi il primo ciclo non entro ma entro al secondo quando counterForPlayerHand = 2
+           e cosi' via.
+         */
+      } else if (gameCounter == 1) {
         game.turns.foreach(hand => {
-          convertBriscolaSeed(hand.briscola)
-          hand.playersHand.foreach(play => {
-            if (user.equals(play._1))
-              play._2.foreach(card => {
-                var actualCard = CARD_PATH + card + CARD_FORMAT
-                cardsListPlayer += actualCard
-              })
-          })
+          counterForPlayerHand += 1
+          if (counterForPlayerHand == counterTurn) {
+            convertBriscolaSeed(hand.briscola) // prendo la briscola di questo turno
+            team1Score = hand.team1Score // prendo il punteggio del primo team
+            team2Score = hand.team2Score // prendo il punteggio del secondo team
+            hand.playersHand.foreach(player => {
+              if (user.equals(player._1))
+                player._2.foreach(card => {
+                  var actualCard = CARD_PATH + card + CARD_FORMAT
+                  cardsListPlayer += actualCard
+                })
+            })
+          }
         })
-
+        counterTurn += 1
+        counterForPlayerHand = 0
         controller.updateGUI(DistributedCard(cardsListPlayer.toList, playersList.head))
         controller.updateGUI(NotifyBriscolaChosen(briscolaChosen))
+
+      } else if (gameCounter == 2) {
+
+
+
+        /**
+          * Terza situazione. Invio del punteggio. Visto che non posso sapere i winner se il punteggio del team1
+          * (di cui faccio parte) e' maggiore del team2, invio come uno degli utenti che hanno vinto lo username
+          * del primo player sia nel campo user che in quello winner1. Altrimenti invio lo username dell'attore
+          * replay che non sara' mai uguale a quello del primo player.
+          */
+      } else if (gameCounter == 3) {
+        if (team1Score > team2Score) controller.updateGUI(ComputePartialGameScore(playersList.head, playersList.head, playersList.head, team1Score, team2Score))
+        else controller.updateGUI(ComputePartialGameScore(user, playersList.head, playersList.head, team1Score, team2Score))
       }
 
 
 
-      i += 1
+
+
+
+      gameCounter += 1
 
   }
+
 
   override
   def getUsername: String = {
@@ -117,10 +136,9 @@ class ReplayActor(override val controller: GameController, username: String, gam
   }
 }
 
-
-
+/*
 object ReplayActorApp extends App {
   val system = ActorSystem("SwapperSystem")
   val swap = system.actorOf(Props[ReplayActor], name = "replayActor")
   swap ! Swap
-}
+}*/
