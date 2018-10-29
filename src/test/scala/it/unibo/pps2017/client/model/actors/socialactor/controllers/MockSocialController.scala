638dc766db9a3a27645992865f84e5c647691b2a
Tirocigno <fede.naldini1@gmail.com>
2018-10-29 18:33:31 +0100

    Removed a lot of warnings

--- src/test/scala/it/unibo/pps2017/client/model/actors/socialactor/controllers/MockSocialController.scala
+++ src/test/scala/it/unibo/pps2017/client/model/actors/socialactor/controllers/MockSocialController.scala
@@ -15,56 +15,56 @@ class MockSocialController extends SocialController {
 
   override var currentActorRef: ActorRef = _
 
-  override def notifyCallResultToGUI(message: Option[String]): Unit = ???
+  override def notifyCallResultToGUI(message: Option[String]): Unit = {}
 
-  override def setOnlinePlayerList(playerList: SocialMap): Unit = ???
+  override def setOnlinePlayerList(playerList: SocialMap): Unit = {}
 
-  override def notifyErrorToGUI(throwable: Throwable): Unit = ???
+  override def notifyErrorToGUI(throwable: Throwable): Unit = {}
 
-  override def registerNewFriend(friendId: PlayerID): Unit = ???
+  override def registerNewFriend(friendId: PlayerID): Unit = {}
 
-  override def updateParty(currentPartyMap: Map[PartyRole, PlayerID]): Unit = ???
+  override def updateParty(currentPartyMap: Map[PartyRole, PlayerID]): Unit = {}
 
-  override def executeFoundGameCall(paramMap: Map[String, String]): Unit = ???
+  override def executeFoundGameCall(paramMap: Map[String, String]): Unit = {}
 
-  override def updateOnlinePlayerList(playerRefList: FriendList): Unit = ???
+  override def updateOnlinePlayerList(playerRefList: FriendList): Unit = {}
 
-  override def updateOnlineFriendsList(friendList: FriendList): Unit = ???
+  override def updateOnlineFriendsList(friendList: FriendList): Unit = {}
 
-  override def createActor(actorID: String, actorSystem: ActorSystem): Unit = ???
+  override def createActor(actorID: String, actorSystem: ActorSystem): Unit = {}
 
-  override def updateGUI(message: ActorMessage): Unit = ???
+  override def updateGUI(message: ActorMessage): Unit = {}
 
   /**
     * Tell the actor to add a new friend.
     *
     * @param playerID the ID of the player to add as a friend.
     */
-  override def tellFriendShipMessage(playerID: PlayerID): Unit = ???
+  override def tellFriendShipMessage(playerID: PlayerID): Unit = {}
 
   /**
     * Tell the actor to invite a player to play as his partner.
     *
     * @param playerID the ID of the player to invite.
     */
-  override def tellInvitePlayerAsPartner(playerID: PlayerID): Unit = ???
+  override def tellInvitePlayerAsPartner(playerID: PlayerID): Unit = {}
 
   /**
     * Tell the actor to invite a player to play as his foe.
     *
     * @param playerID the ID of the player to invite.
     */
-  override def tellInvitePlayerAsFoe(playerID: PlayerID): Unit = ???
+  override def tellInvitePlayerAsFoe(playerID: PlayerID): Unit = {}
 
   /**
     * Start a new game
     *
     * @param matchNature the nature of the game to play.
     */
-  override def startGame(matchNature: MatchNature.MatchNature): Unit = ???
+  override def startGame(matchNature: MatchNature.MatchNature): Unit = {}
 
   /**
     * Reset the party and notify the GUI a match conclusion.
     */
-  override def finishGame(): Unit = ???
+  override def finishGame(): Unit = {}
 }
--- src/test/scala/it/unibo/pps2017/client/model/remote/SocialRestWebClientTest.scala
+++ src/test/scala/it/unibo/pps2017/client/model/remote/SocialRestWebClientTest.scala
@@ -93,55 +93,55 @@ class SocialRestWebClientTest extends FunSuite with BeforeAndAfterEach {
 
     override def setOnlinePlayerList(playerList: SocialMap): Unit = this.playerList = playerList
 
-    override def createActor(actorID: String, actorSystem: ActorSystem): Unit = ???
+    override def createActor(actorID: String, actorSystem: ActorSystem): Unit = {}
 
-    override def updateGUI(message: ActorMessage): Unit = ???
+    override def updateGUI(message: ActorMessage): Unit = {}
 
-    override def notifyErrorToGUI(throwable: Throwable): Unit = ???
+    override def notifyErrorToGUI(throwable: Throwable): Unit = {}
 
 
-    override def registerNewFriend(friendId: PlayerID): Unit = ???
+    override def registerNewFriend(friendId: PlayerID): Unit = {}
 
-    override def updateParty(currentPartyMap: Map[PartyRole, PlayerID]): Unit = ???
+    override def updateParty(currentPartyMap: Map[PartyRole, PlayerID]): Unit = {}
 
-    override def executeFoundGameCall(paramMap: Map[String, String]): Unit = ???
+    override def executeFoundGameCall(paramMap: Map[String, String]): Unit = {}
 
-    override def updateOnlineFriendsList(friendList: FriendList): Unit = ???
+    override def updateOnlineFriendsList(friendList: FriendList): Unit = {}
 
-    override def updateOnlinePlayerList(friendList: FriendList): Unit = ???
+    override def updateOnlinePlayerList(friendList: FriendList): Unit = {}
 
     /**
       * Tell the actor to add a new friend.
       *
       * @param playerID the ID of the player to add as a friend.
       */
-    override def tellFriendShipMessage(playerID: PlayerID): Unit = ???
+    override def tellFriendShipMessage(playerID: PlayerID): Unit = {}
 
     /**
       * Tell the actor to invite a player to play as his partner.
       *
       * @param playerID the ID of the player to invite.
       */
-    override def tellInvitePlayerAsPartner(playerID: PlayerID): Unit = ???
+    override def tellInvitePlayerAsPartner(playerID: PlayerID): Unit = {}
 
     /**
       * Tell the actor to invite a player to play as his foe.
       *
       * @param playerID the ID of the player to invite.
       */
-    override def tellInvitePlayerAsFoe(playerID: PlayerID): Unit = ???
+    override def tellInvitePlayerAsFoe(playerID: PlayerID): Unit = {}
 
     /**
       * Start a new game
       *
       * @param matchNature the nature of the game to play.
       */
-    override def startGame(matchNature: MatchNature.MatchNature): Unit = ???
+    override def startGame(matchNature: MatchNature.MatchNature): Unit = {}
 
     /**
       * Reset the party and notify the GUI a match conclusion.
       */
-    override def finishGame(): Unit = ???
+    override def finishGame(): Unit = {}
   }
 
 
