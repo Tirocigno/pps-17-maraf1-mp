9065b4a95bc39090654b1aa708826f07baed35e0
Gjulio <gjuliojakova@yahoo.com>
2018-10-29 23:50:55 +0100

    Added methods for gui and controller interaction

--- src/main/java/it/unibo/pps2017/core/gui/GenericController.java
+++ src/main/java/it/unibo/pps2017/core/gui/GenericController.java
@@ -1,17 +1,22 @@
 package it.unibo.pps2017.core.gui;
 
+import akka.actor.ActorRef;
 import it.unibo.pps2017.client.controller.ClientController;
 import it.unibo.pps2017.client.controller.ClientController$;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.stage.Stage;
+import java.util.Map;
 
 public class GenericController {
 
     @FXML
     Button playButton;
+    @FXML
+    ListView<String> onlineFriends, onlinePlayers;
 
     private static final int MIN_WIDTH = 900;
     private static final int MIN_HEIGHT = 685;
@@ -43,5 +48,38 @@ public class GenericController {
             e.printStackTrace();
         }
 
+    }
+
+    public void setOnlineFriendsList(Map<String, ActorRef> friendsList){
+            for(String player: friendsList.keySet()){
+                onlineFriends.getItems().add(player);
+            }
+    }
+
+    public void setOnlinePlayersList(Map<String, ActorRef> playerList){
+        for(String player: playerList.keySet()){
+            onlinePlayers.getItems().add(player);
+        }
+    }
+
+    public void addNewFriend(){
+        String newFriend = onlinePlayers.getSelectionModel().getSelectedItem();
+        //addFriend(newFriend);
+    }
+
+    public void updateOnlineFriendsList(){
+        onlineFriends.getItems().clear();
+        Map<String, ActorRef> friendsList = null;//getFriendsList()
+        for(String player: friendsList.keySet()){
+            onlineFriends.getItems().add(player);
+        }
+    }
+
+    public void updateOnlinePlayersList(){
+        onlinePlayers.getItems().clear();
+        Map<String, ActorRef> playerList = null;//getPlayersList()
+        for(String player: playerList.keySet()){
+            onlinePlayers.getItems().add(player);
+        }
     }
 }
--- src/main/java/it/unibo/pps2017/core/gui/GenericView.java
+++ src/main/java/it/unibo/pps2017/core/gui/GenericView.java
@@ -6,6 +6,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class GenericView extends Application {
 
     private static final int MIN_WIDTH = 600;
