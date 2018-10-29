bb1dc5de34122d8b9109ccfa0797e0f3bf1420ee
Nicholas <nicholas.brasini@studio.unibo.it>
2018-10-29 09:44:35 +0100

    Replaced isFirstPlayer with EndPartialTurn.

--- src/main/scala/it/unibo/pps2017/client/model/actors/ReplayActor.scala
+++ src/main/scala/it/unibo/pps2017/client/model/actors/ReplayActor.scala
@@ -1,7 +1,24 @@
 
 package it.unibo.pps2017.client.model.actors
 
-class ReplayActor {
+import it.unibo.pps2017.client.controller.actors.playeractor.GameController
+import it.unibo.pps2017.server.model.Game
 
+class ReplayActor(override val controller: GameController, username: String, game: Game) extends ClientGameActor {
+
+  var user: String = username
+
+
+  def receive: PartialFunction[Any, Unit] = {
+
+    case _ =>
+  }
+
+
+
+  override
+  def getUsername: String = {
+    user
+  }
 
 }
