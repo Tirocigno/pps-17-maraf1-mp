c8f8833083b3da192cfaeae81f64e3b3388c09e0
Nicholas <nicholas.brasini@studio.unibo.it>
2018-10-31 13:04:55 +0100

    Used of PlayGameViewUtils.

--- src/main/java/it/unibo/pps2017/core/gui/PlayGameView.java
+++ src/main/java/it/unibo/pps2017/core/gui/PlayGameView.java
@@ -10,27 +10,23 @@ import javafx.stage.Stage;
 
 public class PlayGameView extends Application {
 
-	private static final int MIN_WIDTH = 900;
-	private static final int MIN_HEIGHT = 685;
-	private static final int DISCOVERY_PORT = 2000;
-
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-            final FXMLLoader loader = new FXMLLoader(PlayGameView.class.getResource("PlayGameView.fxml"));
-            Parent root = loader.load();
+            final FXMLLoader loader;
+			loader = new FXMLLoader(PlayGameView.class.getResource(PlayGameViewUtils.getPlayGameViewFxml()));
+			Parent root = loader.load();
             final PlayGameController gameController = loader.getController();
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setFullScreen(true);
-			primaryStage.setMinHeight(MIN_HEIGHT);
-			primaryStage.setMinWidth(MIN_WIDTH);
+			primaryStage.setMinHeight(PlayGameViewUtils.getMinHeight());
+			primaryStage.setMinWidth(PlayGameViewUtils.getMinWidth());
 			ClientController clientController = ClientController$.MODULE$.getSingletonController();
             clientController.setPlayGameController(gameController);
             gameController.setGameController(clientController.getGameController());
 			clientController.startActorSystem("127.0.0.1", "127.0.0.1");
-			clientController.createRestClient("127.0.0.1", DISCOVERY_PORT);
+			clientController.createRestClient("127.0.0.1", PlayGameViewUtils.getDiscoveryPort());
             clientController.sendMatchRequest();
 			primaryStage.show();
 
