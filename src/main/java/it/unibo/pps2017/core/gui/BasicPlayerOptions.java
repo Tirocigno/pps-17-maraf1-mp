05b80eb96d113315728471ea6151e2f3f35492ac
Gjulio <gjuliojakova@yahoo.com>
2018-11-01 11:15:06 +0100

    Created BasicPlayerOptions class for both logged and not logged in players

---
+++ src/main/java/it/unibo/pps2017/core/gui/BasicPlayerOptions.java
@@ -0,0 +1,16 @@
+package it.unibo.pps2017.core.gui;
+
+public interface BasicPlayerOptions {
+
+    /**
+     * Method that can be called from both logged in and non-logged in player, to play a non competitive match
+     * which will have no effect on ranking
+     * @param competitive if match is competitive or not
+     */
+    void playMatch(boolean competitive);
+
+    /**
+     * Method that can be called from both logged in and non-logged in player, to watch a match without playing
+     */
+    void viewMatch();
+}
