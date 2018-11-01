c21bcce52d79f20ffb9525903dfaf04c2947d7ca
Nicholas <nicholas.brasini@studio.unibo.it>
2018-11-01 17:45:18 +0100

    Refactored PlayerActorClient.

--- src/main/scala/it/unibo/pps2017/client/model/actors/playeractor/PlayerActorClient.scala
+++ src/main/scala/it/unibo/pps2017/client/model/actors/playeractor/PlayerActorClient.scala
@@ -8,13 +8,14 @@ import it.unibo.pps2017.client.controller.actors.playeractor.GameController
 import it.unibo.pps2017.client.model.actors.ClientGameActor
 import it.unibo.pps2017.client.model.actors.playeractor.ClientMessages._
 import it.unibo.pps2017.client.model.actors.playeractor.PlayerActorClient._
+import it.unibo.pps2017.core.deck.cards.Seed.Seed
 
 import scala.collection.mutable.ListBuffer
 
 object PlayerActorClient {
-  val START_SEARCH: Int = 0
-  val FOUNDED: Int = 1
-  val END_SEARCH: Int = 4
+  final val START_SEARCH: Int = 0
+  final val FOUNDED: Int = 1
+  final val END_SEARCH: Int = 4
 }
 
 class PlayerActorClient(override val controller: GameController, username: String) extends ClientGameActor with Stash {
@@ -24,80 +25,144 @@ class PlayerActorClient(override val controller: GameController, username: Strin
   var orderedPlayersList = new ListBuffer[String]()
   var gameActor: ActorRef = _
   var cardArrived: Boolean = false
+  var finalList: ListBuffer[String] = ListBuffer[String]()
 
   def receive: PartialFunction[Any, Unit] = {
 
     case PlayersRef(playersList) =>
       gameActor = sender()
-      var finalList: ListBuffer[String] = ListBuffer[String]()
-      finalList = orderPlayersList(playersList)
-      controller.updateGUI(PlayersRef(finalList))
-      gameActor ! PlayersRefAck
+      communicatePlayersMatch(playersList)
 
     case DistributedCard(cards, player) =>
-      if (user.equals(player)) {
-        controller.updateGUI(DistributedCard(cards, player))
-        cardArrived = true
-        unstashAll()
-      }
+      communicatePlayersCard(cards, player)
 
     case SelectBriscola(player) =>
-      if (user.equals(player))
-        if (!cardArrived) {
-          stash()
-        } else {
-          controller.updateGUI(SelectBriscola(player))
-          cardArrived = false
-        }
+      informPlayerToChooseBriscola(player)
 
     case BriscolaChosen(seed) =>
-      gameActor ! BriscolaChosen(seed)
+      sendBriscolaChosen(seed)
 
     case NotifyBriscolaChosen(seed) =>
-      controller.updateGUI(NotifyBriscolaChosen(seed))
-      gameActor ! BriscolaAck
+      notifyBriscolaChosen(seed)
 
     case ClickedCardActualPlayer(index) =>
-      gameActor ! ClickedCard(index, user)
+      sendIndexClickedCard(index)
 
     case CardOk(correctClickedCard, player) =>
-      if (user.equals(player))
-        controller.updateGUI(CardOk(correctClickedCard, player))
+      cardOk(correctClickedCard, player)
 
     case ClickedCommandActualPlayer(command) =>
-      gameActor ! ClickedCommand(command, user)
+      sendClickedCommand(command)
 
     case Turn(player, endPartialTurn, isFirstPlayer) =>
-      if (user.equals(player)) {
-        controller.setMyTurn(true)
-      } else {
-        controller.setMyTurn(false)
-      }
-      controller.updateGUI(Turn(player, endPartialTurn, isFirstPlayer))
+      communicateTurn(player, endPartialTurn, isFirstPlayer)
 
     case PlayedCard(card, player) =>
-      controller.updateGUI(PlayedCard(card, player))
-      gameActor ! CardPlayedAck
+      communicatePlayedCard(card, player)
 
     case NotifyCommandChosen(command, player) =>
-      controller.updateGUI(NotifyCommandChosen(command, player))
+      notifyCommandChosen(command, player)
 
     case PartialGameScore(winner1, winner2, score1, score2) =>
-      controller.updateGUI(ComputePartialGameScore(user, winner1, winner2, score1, score2))
+      communicatePartialGameScore(winner1, winner2, score1, score2)
 
     case FinalGameScore(winner1, winner2, score1, score2) =>
-      controller.updateGUI(ComputeFinalGameScore(user, winner1, winner2, score1, score2))
+      communicateFinalGameScore(winner1, winner2, score1, score2)
 
     case SetUsernamePlayer(playerUsername) =>
-      user = playerUsername
+      setUsername(playerUsername)
 
     case IdChannelPublishSubscribe(id) =>
-      val mediator = DistributedPubSub(context.system).mediator
-      mediator ! Subscribe(id, self)
+      registerToChannel(id)
 
     case NotifyClosedPlayGameView() =>
-      gameActor ! ClosedPlayGameView(user)
-      self ! PoisonPill
+      notifyClosedGame()
+  }
+
+  private def communicatePlayersMatch(playersList: ListBuffer[String]): Unit = {
+    finalList = orderPlayersList(playersList)
+    controller.updateGUI(PlayersRef(finalList))
+    gameActor ! PlayersRefAck
+  }
+
+  private def communicatePlayersCard(cards: List[String], player: String): Unit = {
+    if (user.equals(player)) {
+      controller.updateGUI(DistributedCard(cards, player))
+      cardArrived = true
+      unstashAll()
+    }
+  }
+
+  private def informPlayerToChooseBriscola(player: String): Unit = {
+    if (user.equals(player))
+      if (!cardArrived) {
+        stash()
+      } else {
+        controller.updateGUI(SelectBriscola(player))
+        cardArrived = false
+      }
+  }
+
+  private def sendBriscolaChosen(seed: Seed): Unit = {
+    gameActor ! BriscolaChosen(seed)
+  }
+
+  private def notifyBriscolaChosen(seed: Seed): Unit = {
+    controller.updateGUI(NotifyBriscolaChosen(seed))
+    gameActor ! BriscolaAck
+  }
+
+  private def sendIndexClickedCard(index: Int): Unit = {
+    gameActor ! ClickedCard(index, user)
+  }
+
+  private def cardOk(correctClickedCard: Boolean, player: String): Unit = {
+    if (user.equals(player))
+      controller.updateGUI(CardOk(correctClickedCard, player))
+  }
+
+  private def sendClickedCommand(command: String): Unit = {
+    gameActor ! ClickedCommand(command, user)
+  }
+
+  private def communicateTurn(player: String, endPartialTurn: Boolean, isFirstPlayer: Boolean): Unit = {
+    if (user.equals(player)) {
+      controller.setMyTurn(true)
+    } else {
+      controller.setMyTurn(false)
+    }
+    controller.updateGUI(Turn(player, endPartialTurn, isFirstPlayer))
+  }
+
+  private def communicatePlayedCard(card: String, player: String): Unit = {
+    controller.updateGUI(PlayedCard(card, player))
+    gameActor ! CardPlayedAck
+  }
+
+  private def notifyCommandChosen(command: String, player: String): Unit = {
+    controller.updateGUI(NotifyCommandChosen(command, player))
+  }
+
+  private def communicatePartialGameScore(winner1: String, winner2: String, score1: Int, score2: Int): Unit = {
+    controller.updateGUI(ComputePartialGameScore(user, winner1, winner2, score1, score2))
+  }
+
+  private def communicateFinalGameScore(winner1: String, winner2: String, score1: Int, score2: Int): Unit =
+    controller.updateGUI(ComputePartialGameScore(user, winner1, winner2, score1, score2))
+
+
+  private def setUsername(playerUsername: String): Unit =
+    user = playerUsername
+
+
+  private def registerToChannel(id: String): Unit = {
+    val mediator = DistributedPubSub(context.system).mediator
+    mediator ! Subscribe(id, self)
+  }
+
+  private def notifyClosedGame(): Unit = {
+    gameActor ! ClosedPlayGameView(user)
+    self ! PoisonPill
   }
 
   private def orderPlayersList(playersList: ListBuffer[String]): ListBuffer[String] = {
