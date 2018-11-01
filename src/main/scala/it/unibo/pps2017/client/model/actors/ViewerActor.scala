cbd4b2f6c298aa1653b74583fcf17d4d4efff655
Nicholas <nicholas.brasini@studio.unibo.it>
2018-11-01 17:54:22 +0100

    Refactored ViewerActor.

--- src/main/scala/it/unibo/pps2017/client/model/actors/ViewerActor.scala
+++ src/main/scala/it/unibo/pps2017/client/model/actors/ViewerActor.scala
@@ -5,6 +5,9 @@ import akka.cluster.pubsub.DistributedPubSub
 import akka.cluster.pubsub.DistributedPubSubMediator.Subscribe
 import it.unibo.pps2017.client.controller.actors.playeractor.GameController
 import it.unibo.pps2017.client.model.actors.playeractor.ClientMessages._
+import it.unibo.pps2017.core.deck.cards.Seed.Seed
+
+import scala.collection.mutable.ListBuffer
 
 class ViewerActor(override val controller: GameController, username: String) extends ClientGameActor {
 
@@ -17,47 +20,74 @@ class ViewerActor(override val controller: GameController, username: String) ext
   def receive: PartialFunction[Any, Unit] = {
 
     case IdChannelPublishSubscribe(id) =>
-      val mediator = DistributedPubSub(context.system).mediator
-      mediator ! Subscribe(id, self)
+      registerToChannel(id)
 
     case RecapActualSituation(playersList, cards, seed, player) =>
-      controller.updateGUI(PlayersRef(playersList))
-      firstPlayer = playersList.head
-      controller.updateGUI(DistributedCard(cards.toList, firstPlayer))
-      controller.updateGUI(NotifyBriscolaChosen(seed = seed))
-      controller.updateGUI(Turn(player, endPartialTurn = true, isFirstPlayer = false))
+      communicateRecapActualSituation(playersList, cards, seed, player)
 
       become({
 
         case DistributedCard(cardsOfPlayer, actualPlayer) =>
-          if (actualPlayer.equals(firstPlayer))
-            controller.updateGUI(DistributedCard(cardsOfPlayer, actualPlayer))
+          communicatePlayersCard(cardsOfPlayer, actualPlayer)
 
         case NotifyBriscolaChosen(seedChosen) =>
-          controller.updateGUI(NotifyBriscolaChosen(seed = seedChosen))
+          notifyBriscolaChosen(seedChosen)
 
         case Turn(actualPlayer, endPartialTurn, _) =>
-          controller.updateGUI(Turn(actualPlayer, endPartialTurn, isFirstPlayer = false))
+          communicateTurn(actualPlayer, endPartialTurn)
 
         case PlayedCard(card, actualPlayer) =>
-          controller.updateGUI(PlayedCard(card, actualPlayer))
+          communicatePlayedCard(card, actualPlayer)
 
         case NotifyCommandChosen(command, actualPlayer) =>
-          controller.updateGUI(NotifyCommandChosen(command, actualPlayer))
+          notifyCommandChosen(command, actualPlayer)
 
         case PartialGameScore(winner1, winner2, score1, score2) =>
-          controller.updateGUI(ComputePartialGameScore(firstPlayer, winner1, winner2, score1, score2))
+          communicatePartialGameScore(winner1, winner2, score1, score2)
 
         case FinalGameScore(winner1, winner2, score1, score2) =>
-          controller.updateGUI(ComputeFinalGameScore(firstPlayer, winner1, winner2, score1, score2))
+          communicateFinalGameScore(winner1, winner2, score1, score2)
+
 
       }, discardOld = true)
   }
 
-
-  override
-  def getUsername: String = {
-    user
+  private def registerToChannel(id: String): Unit = {
+    val mediator = DistributedPubSub(context.system).mediator
+    mediator ! Subscribe(id, self)
   }
 
+  private def communicateRecapActualSituation(playersList: ListBuffer[String], cards: ListBuffer[String],
+                                              seed: Seed, player: String): Unit = {
+    controller.updateGUI(PlayersRef(playersList))
+    firstPlayer = playersList.head
+    controller.updateGUI(DistributedCard(cards.toList, firstPlayer))
+    controller.updateGUI(NotifyBriscolaChosen(seed = seed))
+    controller.updateGUI(Turn(player, endPartialTurn = true, isFirstPlayer = false))
+  }
+
+  private def communicatePlayersCard(cardsOfPlayer: List[String], actualPlayer: String): Unit =
+    if (actualPlayer.equals(firstPlayer))
+      controller.updateGUI(DistributedCard(cardsOfPlayer, actualPlayer))
+
+  private def notifyBriscolaChosen(seedChosen: Seed): Unit =
+    controller.updateGUI(NotifyBriscolaChosen(seed = seedChosen))
+
+  private def communicateTurn(actualPlayer: String, endPartialTurn: Boolean): Unit =
+    controller.updateGUI(Turn(actualPlayer, endPartialTurn, isFirstPlayer = false))
+
+  private def communicatePlayedCard(card: String, actualPlayer: String): Unit =
+    controller.updateGUI(PlayedCard(card, actualPlayer))
+
+  private def notifyCommandChosen(command: String, actualPlayer: String): Unit =
+    controller.updateGUI(NotifyCommandChosen(command, actualPlayer))
+
+  private def communicatePartialGameScore(winner1: String, winner2: String, score1: Int, score2: Int): Unit =
+    controller.updateGUI(ComputePartialGameScore(firstPlayer, winner1, winner2, score1, score2))
+
+  private def communicateFinalGameScore(winner1: String, winner2: String, score1: Int, score2: Int): Unit =
+    controller.updateGUI(ComputeFinalGameScore(firstPlayer, winner1, winner2, score1, score2))
+
+  override
+  def getUsername: String = user
 }
