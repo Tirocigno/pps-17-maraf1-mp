ec8d5d82f32dc860d5b92607d7c1604186f70910
Tirocigno <fede.naldini1@gmail.com>
2018-11-01 11:43:14 +0100

    Deployed methods inside ClientController

--- src/main/scala/it/unibo/pps2017/client/controller/ClientController.scala
+++ src/main/scala/it/unibo/pps2017/client/controller/ClientController.scala
@@ -5,8 +5,9 @@ import akka.actor.ActorSystem
 import it.unibo.pps2017.client.controller.actors.playeractor.GameController
 import it.unibo.pps2017.client.controller.socialcontroller.SocialController
 import it.unibo.pps2017.client.model.remote.{GameRestWebClient, RestWebClient}
-import it.unibo.pps2017.client.view.{GenericGUIController, GuiStack}
+import it.unibo.pps2017.client.view.{GameStage, GenericGUIController, GuiStack}
 import it.unibo.pps2017.commons.remote.akka.AkkaClusterUtils
+import it.unibo.pps2017.commons.remote.rest.API.RestAPI
 import it.unibo.pps2017.commons.remote.rest.RestUtils.{IPAddress, MatchRef, Port, ServerContext}
 import it.unibo.pps2017.server.model.ServerApi.FoundGameRestAPI
 
@@ -46,8 +47,10 @@ sealed trait ClientController extends Controller {
 
   /**
     * Send a match request to the server.
+    *
+    * @param paramMap a map containing party parameters, if present.
     */
-  def sendMatchRequest(): Unit
+  def sendMatchRequest(paramMap: Option[Map[String, String]]): Unit
 
   /**
     * Start the game GUI and notify GameActor and SocialActor the player has joined a match.
@@ -141,6 +144,7 @@ object ClientController {
     var actorSystem: Option[ActorSystem] = None
     var webClient: Option[RestWebClient] = None
     var socialController: Option[SocialController] = None
+    var genericGui: GenericGUIController = _
 
     override def notifyError(throwable: Throwable): Unit = {
       throwable.printStackTrace()
@@ -161,19 +165,86 @@ object ClientController {
       webClient = Some(GameRestWebClient(ServerContext(discoveryIP, discoveryPort)))
     }
 
-    override def sendMatchRequest(): Unit = {
-      val map = Map(FoundGameRestAPI.meParamKey -> playerName)
-      webClient.get.callRemoteAPI(FoundGameRestAPI, Some(map))
+    override def sendMatchRequest(paramMap: Option[Map[String, String]]): Unit = paramMap match {
+      case Some(_) => webClient.get.callRemoteAPI(FoundGameRestAPI, paramMap)
+      case None => val map = Map(FoundGameRestAPI.meParamKey -> playerName)
+        webClient.get.callRemoteAPI(FoundGameRestAPI, Some(map))
     }
 
 
-    override def setCurrentGUI(gui: GenericGUIController): Unit = ???
+    override def setCurrentGUI(gui: GenericGUIController): Unit = genericGui = gui
+
+
+    override def handleMatchResponse(gameID: String): Unit = {
+      guiStack.setCurrentScene(GameStage, gameController)
+      gameController.joinPlayerToMatch(gameID)
+      socialController match {
+        case Some(controller) => controller //TODO INVOKE METHOD ON SOCIALCONTROLLER
+        case None =>
+      }
+    }
+
+    override def sendLoginRequest(userName: String, password: String): Unit = ???
 
     /**
-      * Start the game GUI and notify GameActor and SocialActor the player has joined a match.
+      * Handle a login response.
       *
-      * @param gameID a string containing the gameID, notify by the server.
+      * @param userName the userName notify by the server.
       */
-    override def handleMatchResponse(gameID: String): Unit = ???
+    override def handleLoginAndRegistrationResponse(userName: String): Unit = ???
+
+    /**
+      * Send a registration request to remote server.
+      *
+      * @param userName a string containing the username of the player to register.
+      * @param password a string containing the password of the player to register.
+      */
+    override def sendRegisterRequest(userName: String, password: String): Unit = ???
+
+    /**
+      * Fetch current played matches from server.
+      */
+    override def fetchCurrentMatchesList(): Unit = ???
+
+    /**
+      * Display current played matches.
+      *
+      * @param playedMatches a list containing all matchIDs as strings.
+      */
+    override def displayCurrentMatchesList(playedMatches: List[MatchRef]): Unit = ???
+
+    /**
+      * Fetch the archive of matches played from a server.
+      */
+    override def fetchRegisteredMatchesList(): Unit = ???
+
+    /**
+      * Display the archive of matches.
+      *
+      * @param playedMatches a list containing all matchIDs as String
+      */
+    override def displayRegisteredMatchesList(playedMatches: List[MatchRef]): Unit = ???
+
+    /**
+      * Start watching a current played match.
+      *
+      * @param matchID ID of the match to watch.
+      */
+    override def startMatchWatching(matchID: String): Unit = ???
+
+    /**
+      * Start replay a played match.
+      *
+      * @param matchID ID of the match to replay.
+      */
+    override def startMatchReplay(matchID: String): Unit = ???
+
+    /**
+      * Notify to whole system that a game is finished.
+      */
+    override def notifyGameFinished(): Unit = ???
+
+    //TODO DEPLOY THIS METHOD
+    private def executeAutenticationApiCall(userName: String, password: String, api: RestAPI): Unit = ???
   }
 }
