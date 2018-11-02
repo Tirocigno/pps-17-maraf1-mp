9ca2e016be15f41f1985b93baebdfd28de0c7a3f
Tirocigno <fede.naldini1@gmail.com>
2018-11-02 16:49:18 +0100

    Don't panic

--- src/main/scala/it/unibo/pps2017/client/ClientMain.scala
+++ src/main/scala/it/unibo/pps2017/client/ClientMain.scala
@@ -1,32 +1,21 @@
 
 package it.unibo.pps2017.client
 
-import it.unibo.pps2017.client.controller.actors.playeractor.GameController
 import it.unibo.pps2017.client.controller.clientcontroller.ClientController
 import it.unibo.pps2017.client.view.GuiStack
 import it.unibo.pps2017.commons.remote.game.MatchNature.CasualMatch
-import it.unibo.pps2017.discovery.DiscoveryMain.args
 import javafx.application.Application
 import javafx.stage.Stage
 import org.rogach.scallop.ScallopConf
 
 class ClientMain extends Application {
-  val matchController = new GameController()
-  val clientController: ClientController = ClientController.getSingletonController
 
   override def start(primaryStage: Stage): Unit = {
     GuiStack().checkAndSetStage(primaryStage)
     //GuiStack().setCurrentScene(GameStage, matchController)
-    startFoundGameRequest()
     primaryStage.show()
   }
 
-  private def startFoundGameRequest(): Unit = {
-    val conf = new Conf(args)
-    clientController.startActorSystem(conf.discoveryip(), conf.myip())
-    clientController.createRestClient(conf.discoveryip(), conf.myport())
-    clientController.sendMatchRequest(CasualMatch, None)
-  }
 }
 
 class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
@@ -37,5 +26,10 @@ class Conf(arguments: Seq[String]) extends ScallopConf(arguments) {
 }
 
 object ClientMain extends App {
+  val conf = new Conf(args)
     Application.launch(classOf[ClientMain], args: _*)
+  val clientController: ClientController = ClientController.getSingletonController
+  clientController.startActorSystem(conf.discoveryip(), conf.myip())
+  clientController.createRestClient(conf.discoveryip(), conf.myport())
+  clientController.sendMatchRequest(CasualMatch, None)
 }
