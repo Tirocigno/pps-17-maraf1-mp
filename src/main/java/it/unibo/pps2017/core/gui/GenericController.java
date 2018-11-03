93ca8c6b134f06889191bd3a0f95668b090c0975
Gjulio <gjuliojakova@yahoo.com>
2018-11-03 15:08:51 +0100

    Implemented GenericController methods

--- src/main/java/it/unibo/pps2017/core/gui/GenericController.java
+++ src/main/java/it/unibo/pps2017/core/gui/GenericController.java
@@ -1,45 +1,31 @@
 package it.unibo.pps2017.core.gui;
 
+import it.unibo.pps2017.client.controller.clientcontroller.ClientController;
+import it.unibo.pps2017.client.controller.clientcontroller.ClientController$;
+import it.unibo.pps2017.commons.remote.game.MatchNature;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.control.*;
-import javafx.stage.Stage;
-
-import java.util.ArrayList;
 import java.util.List;
 
-public class GenericController implements BasicPlayerOptions{
+public class GenericController implements  BasicPlayerOptions{
 
     @FXML
     Button playButton, viewButton, goButton;
     @FXML
     ListView<String> matchesList;
 
-    private static final int MIN_WIDTH = 900;
-    private static final int MIN_HEIGHT = 685;
-    private static final int DISCOVERY_PORT = 2000;
+    private ClientController clientController = ClientController$.MODULE$.getSingletonController();
 
     @FXML
     private void handlePlayMatch(){
         playMatch(false);
-        //set non competitive msg
     }
 
     @Override
     public void playMatch(boolean competitive) {
-        Stage primaryStage = (Stage) playButton.getScene().getWindow();
-        primaryStage.setMinHeight(MIN_HEIGHT);
-        primaryStage.setMinWidth(MIN_WIDTH);
-
-        try {
-            final FXMLLoader loader = new FXMLLoader(PlayGameView.class.getResource("PlayGameView.fxml"));
-            Parent root = loader.load();
-            primaryStage.getScene().setRoot(root);
-            primaryStage.centerOnScreen();
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        scala.Option paramMap = scala.Option.apply(null);
+        if(!competitive){
+            clientController.sendMatchRequest(MatchNature.CasualMatch$.MODULE$, paramMap);
         }
     }
 
@@ -49,14 +35,18 @@ public class GenericController implements BasicPlayerOptions{
 
     @Override
     public void watchMatch() {
-        //api call
-        List<String> matches = new ArrayList<>();
+        clientController.fetchCurrentMatchesList();
+    }
+
+    public void displayViewMatches(List<String> matches){
         matchesList.getItems().clear();
         matchesList.getItems().addAll(matches);
     }
 
     public void goViewMatch(){
         String matchSelected = matchesList.getSelectionModel().getSelectedItem();
-        // viewMatch(matchSelected);
+        clientController.startMatchWatching(matchSelected);
     }
+
+
 }
