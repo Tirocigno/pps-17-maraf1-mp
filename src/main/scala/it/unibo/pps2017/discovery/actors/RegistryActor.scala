b83033d384c202246619e497535da150cec541d6
Tirocigno <fede.naldini1@gmail.com>
2018-11-04 17:57:35 +0100

    Created registryActor

---
+++ src/main/scala/it/unibo/pps2017/discovery/actors/RegistryActor.scala
@@ -0,0 +1,57 @@
+
+package it.unibo.pps2017.discovery.actors
+
+import akka.actor.{Actor, ActorRef}
+import akka.cluster.pubsub.DistributedPubSub
+import akka.cluster.pubsub.DistributedPubSubMediator.Publish
+import it.unibo.pps2017.commons.remote.social.SocialUtils.{PlayerID, SocialMap}
+import it.unibo.pps2017.discovery.actors.RegistryActor.{AddUserToRegisterMessage, OnlinePlayerListMessage, RemoveUserFromRegisterMessage}
+import it.unibo.pps2017.discovery.structures.SocialActorsMap
+
+/**
+  * Registry actor to supply the currentOnlinePlayer list in real time.
+  */
+class RegistryActor extends Actor {
+
+  val mediator: ActorRef = DistributedPubSub(context.system).mediator
+  val socialActorsMap: SocialActorsMap = SocialActorsMap()
+
+  override def receive: Receive = {
+    case AddUserToRegisterMessage(playerID, actorRef) => socialActorsMap.registerUser(playerID, actorRef)
+    case RemoveUserFromRegisterMessage(playerID) => socialActorsMap.unregisterUser(playerID)
+  }
+
+  private def notifyListUpdate(): Unit = {
+    mediator ! Publish(RegistryActor.SOCIALCHANNEL, OnlinePlayerListMessage(socialActorsMap.getCurrentOnlinePlayerMap))
+  }
+}
+
+object RegistryActor {
+  /**
+    * ID of the channel on which the messages will be sent.
+    */
+  val SOCIALCHANNEL = "ONLINEPLAYERSREGISTRY"
+
+  /**
+    * Tell the actor to add a new player.
+    *
+    * @param userID the id of the user to add.
+    * @param sender the actorRef of the sender.
+    */
+  case class AddUserToRegisterMessage(userID: PlayerID, sender: ActorRef)
+
+  /**
+    * Remove a user from the remote list.
+    *
+    * @param userID the id of the user to remove
+    */
+  case class RemoveUserFromRegisterMessage(userID: PlayerID)
+
+  /**
+    * Return the current online players list.
+    *
+    * @param map the map of current online players
+    */
+  case class OnlinePlayerListMessage(map: SocialMap)
+
+}
